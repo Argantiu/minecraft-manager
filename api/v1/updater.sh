a7f03cc6b8228cd10090e893e6f81631963af2ea
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-15 16:22:18 +0200

    Update updater.sh

--- api/v1/updater.sh
+++ api/v1/updater.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 # Updating everything... Made by Argantiu
 # Don't edit this here
-wget -q 
-wget -q
-wget -q
+. ../config/mcsys.conf
+# Path generating
+LPATH=/$OPTBASE/$SERVERBASE
+#
+apt-get -qq update
+cd $LPATH/mcsys || exit 1
 wget -q https://raw.githubusercontent.com/Argantiu/system-api/main/api/v1/restart.sh -O restart.sh
-cd $LPATH
+wget -q https://raw.githubusercontent.com/Argantiu/system-api/main/api/v1/start.sh -O start.sh
+wget -q https://raw.githubusercontent.com/Argantiu/system-api/main/api/v1/stop.sh -O stop.sh
+chmod +x *.sh
+cd $LPATH/mcsys/updater || exit 1
 wget -q https://raw.githubusercontent.com/Argantiu/system-api/main/api/v1/updater.sh -O updater.sh
+exit 1
+
