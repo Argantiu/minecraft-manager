97c65c8200dbb4e88d4506c055540e6487d539d5
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-15 15:16:00 +0200

    Update server-installer.sh

--- api/v1/server-installer.sh
+++ api/v1/server-installer.sh
@@ -13,30 +13,32 @@ echo -e "$PREFIX 2 = German"
 echo -n -e "$PREFIX Please type a number and hit space:";
 read;
 if [ ${REPLY} = "1" ]; then
-echo -e "$PREFIX Everything will be in english"
-echo -e "$PREFIX Were should be or is your server dictionary?"
-echo -e "$PREFIX e.g. /opt/Paper or /home/myserver/server"
-echo -e "$PREFIX Don't write this: /opt/Paper\033[0;31m/ <- You don't need / at the end"
-echo -n -e "$PREFIX Your server dictionary:"
-read;
-echo -e "$PREFIX Okey, i will install now everything. Please wait..."
-sleep 5
-mkdir -p {REPLY}
-mkdir -p {REPLY}/mcsys
-mkdir -p {REPLY}/mcsys/config
-mkdir -p {REPLY}/mcsys/commands
-mkdir -p {REPLY}/unused
-cd {REPLY}/mcsys/config || exit 1
-wget -q https://raw.githubusercontent.com/Argantiu/system-api/main/api/v1/lang/en/mcsys.conf -O mcsys.conf
-cd {REPLY}/mcsys/ || exit 1
-wget -q https://raw.githubusercontent.com/Argantiu/system-api/main/api/v1/restart.sh -O restart.sh
-wget -q 
-wget -q
-wget -q
+ echo -e "$PREFIX Everything will be in english"
+ echo -e "$PREFIX Were should be or is your server dictionary?"
+ echo -e "$PREFIX e.g. /opt/Paper or /home/myserver/server"
+ echo -e "$PREFIX Don't write this: /opt/Paper\033[0;31m/ <- You don't need / at the end"
+ {
+ echo -n -e "$PREFIX Your server dictionary:"
+ read;
+ echo -e "$PREFIX Okey, i will install now everything. Please wait..."
+ sleep 5
+ mkdir -p {REPLY}
+ mkdir -p {REPLY}/mcsys
+ mkdir -p {REPLY}/mcsys/config
+ mkdir -p {REPLY}/mcsys/commands
+ mkdir -p {REPLY}/unused
+ cd {REPLY}/mcsys/config || exit 1
+ wget -q https://raw.githubusercontent.com/Argantiu/system-api/main/api/v1/lang/en/mcsys.conf -O mcsys.conf
+ cd {REPLY}/mcsys/ || exit 1
+ wget -q https://raw.githubusercontent.com/Argantiu/system-api/main/api/v1/restart.sh -O restart.sh
+ wget -q 
+ wget -q
+ wget -q
+ }
 if [ ${REPLY} = "2" ]; then
 echo -e "$PREFIX Vieles wird auf Deutsch sein, jedoch können einige Ausgaben nicht übersetzt werden."
 echo -e "$PREFIX"
-}
+
 # Were is or should be your minecraft server
 
 # setup in english or german
