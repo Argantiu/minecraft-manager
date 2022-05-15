20f4905109027a6abe1c84e6992e5105706c4406
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-15 15:00:37 +0200

    Update updater.sh

--- api/v1/updater.sh
+++ api/v1/updater.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # Updating everything... Made by Argantiu
 # Don't edit this here
+wget -q 
+wget -q
+wget -q
+wget -q https://raw.githubusercontent.com/Argantiu/system-api/main/api/v1/restart.sh -O restart.sh
+cd $LPATH
+wget -q https://raw.githubusercontent.com/Argantiu/system-api/main/api/v1/updater.sh -O updater.sh
