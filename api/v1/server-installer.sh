f5d9d65e44644da52b78a7c79ee7a8bfa1bbf53d
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-15 16:11:53 +0200

    Update server-installer.sh

--- api/v1/server-installer.sh
+++ api/v1/server-installer.sh
@@ -79,4 +79,52 @@ if [ ${REPLY} = "2" ]; then
  }
 fi
 }
+#
+ if ! command -v joe &> /dev/null
+ then
+     apt-get install joe -y
+     echo "joe installed"
+ fi
+#
+ if ! command -v screen &> /dev/null
+ then
+     apt-get install screen -y
+     echo "screen installed"
+ fi
+#
+ if ! command -v sudo &> /dev/null
+ then
+     apt-get install sudo -y
+     echo "sudo installed"
+ fi
+#
+ if ! command -v zip &> /dev/null
+ then
+     apt-get install zip -y
+     echo "zip installed"
+ fi
+#
+ if ! command -v wget &> /dev/null
+ then
+     apt-get install wget -y
+     echo "wget installed"
+ fi
+#
+ if ! command -v xargs &> /dev/null
+ then
+     apt-get install findutils -y
+     echo "findutils installed"
+ fi
+#
+ if ! command -v diff &> /dev/null
+ then
+     apt-get install diffutils -y
+     echo "diffutils installed"
+ fi
+#
+ if ! command -v rpl &> /dev/null
+ then
+     apt-get install rpl -y
+     echo "rpl installed"
+ fi
 # End of file. Please sh server-installer.sh this file.
