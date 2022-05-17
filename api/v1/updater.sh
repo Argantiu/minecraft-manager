61e16b2816e5b16a159c156b254490a550247be8
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-17 14:56:45 +0200

    Added Values updater

--- api/v1/updater.sh
+++ api/v1/updater.sh
@@ -2,10 +2,16 @@
 # Updating everything... Made by Argantiu
 # Don't edit this here
 . /config/mcsys.conf
+. /config/values.conf.old 
 # Path generating
 LPATH=/$OPTBASE/$SERVERBASE
 #
-apt-get -qq update
+apt-get -qq upgrade -y
+apt-get -qq update -y
+cd $LPATH/mcsys/config || exit 1
+mv values.conf values.conf.old
+wget -q https://raw.githubusercontent.com/Argantiu/system-api/main/api/v1/values.conf -O values.conf
+sed -i 's/empty/"$DSERVERFOLDER"/g' ./values.conf >/dev/null 2>&1
 cd $LPATH/mcsys || exit 1
 wget -q https://raw.githubusercontent.com/Argantiu/system-api/main/api/v1/restart.sh -O restart.sh
 wget -q https://raw.githubusercontent.com/Argantiu/system-api/main/api/v1/start.sh -O start.sh
