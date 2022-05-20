ac508faaf4ab427245a7761fde9724a534178386
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-20 09:12:14 +0200

    Update paper.sh

--- api/v1/software/paper.sh
+++ api/v1/software/paper.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 #Paper: Getting Update form your selected version.
-if [ $ASOFTWARE = "PAPER" ]; then
- mkdir -p $LPATH/mcsys/jar
- cd $LPATH/mcsys/jar || exit 1
- rm -f version.json
- wget -q https://papermc.io/api/v2/projects/paper/versions/$MAINVERSION/ -O version.json
- LATEST=$(cat < version.json | jq -r ".builds" | grep -v "," | grep -e "[0-9]" | tr -d " ")
- wget -q https://papermc.io/api/v2/projects/paper/versions/$MAINVERSION/builds/$LATEST/downloads/paper-$MAINVERSION-$LATEST.jar -O paper-$MAINVERSION-$LATEST.jar
- unzip -qq -t paper-$MAINVERSION-$LATEST.jar
- if [ "$?" -ne 0 ]; then
-  echo "Downloaded paper-$MAINVERSION-$LATEST.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
+mkdir -p $LPATH/mcsys/jar
+cd $LPATH/mcsys/jar || exit 1
+rm -f version.json
+wget -q https://papermc.io/api/v2/projects/paper/versions/$MAINVERSION/ -O version.json
+LATEST=$(cat < version.json | jq -r ".builds" | grep -v "," | grep -e "[0-9]" | tr -d " ")
+wget -q https://papermc.io/api/v2/projects/paper/versions/$MAINVERSION/builds/$LATEST/downloads/paper-$MAINVERSION-$LATEST.jar -O paper-$MAINVERSION-$LATEST.jar
+unzip -qq -t paper-$MAINVERSION-$LATEST.jar
+if [ "$?" -ne 0 ]; then
+ echo "Downloaded paper-$MAINVERSION-$LATEST.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
+else
+ diff -q paper-$MAINVERSION-$LATEST.jar $LPATH/$MCNAME.jar >/dev/null 2>&1
+ if [ "$?" -eq 1 ]; then
+  cp paper-$MAINVERSION-$LATEST.jar paper-$MAINVERSION-$LATEST.jar."$(date +%Y.%m.%d.%H.%M.%S)"
+  mv paper-$MAINVERSION-$LATEST.jar $LPATH/$MCNAME.jar
+  /usr/bin/find $LPATH/mcsys/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+  echo "paper-$MAINVERSION-$LATEST has been updated" | /usr/bin/logger -t $MCNAME
+  rm version.json
  else
-  diff -q paper-$MAINVERSION-$LATEST.jar $LPATH/$MCNAME.jar >/dev/null 2>&1
-  if [ "$?" -eq 1 ]; then
-   cp paper-$MAINVERSION-$LATEST.jar paper-$MAINVERSION-$LATEST.jar."$(date +%Y.%m.%d.%H.%M.%S)"
-   mv paper-$MAINVERSION-$LATEST.jar $LPATH/$MCNAME.jar
-   /usr/bin/find $LPATH/mcsys/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
-   echo "paper-$MAINVERSION-$LATEST has been updated" | /usr/bin/logger -t $MCNAME
-   rm version.json
-  else
-   echo "No paper-$MAINVERSION-$LATEST update neccessary" | /usr/bin/logger -t $MCNAME
-   rm paper-$MAINVERSION-$LATEST.jar
-   rm version.json
-  fi
+  echo "No paper-$MAINVERSION-$LATEST update neccessary" | /usr/bin/logger -t $MCNAME
+  rm paper-$MAINVERSION-$LATEST.jar
+  rm version.json
  fi
 fi
