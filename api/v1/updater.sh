736293d7e6719681541d5f66f48e4059073e7dbc
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-05-23 22:04:34 +0200

    Update updater.sh

--- api/v1/updater.sh
+++ api/v1/updater.sh
@@ -2,17 +2,17 @@
 # Updating everything... Made by Argantiu
 # Don't edit this here
 # shellcheck source=/lang/en/mcsys.conf
-source ./../config/mcsys.conf
+. /../config/mcsys.txt
 # shellcheck source=/dev/null
-source ./../config/values.conf.old 
+. /../config/values.txt.old 
 # Path generating
 LPATH=/$OPTBASE/$SERVERBASE
 #
 apt-get -qq update -y
 cd $LPATH/mcsys/config || exit 1
-mv values.conf values.conf.old
-wget -q https://raw.githubusercontent.com/Argantiu/system-api/main/api/v1/values.conf -O values.conf
-sed -i "s:empty:$DSERVERFOLDER:g" ./values.conf $>/dev/null 2>&1
+mv values.txt values.txt.old
+wget -q https://raw.githubusercontent.com/Argantiu/system-api/main/api/v1/values.txt -O values.txt
+sed -i "s:empty:$DSERVERFOLDER:g" ./values.txt $>/dev/null 2>&1
 cd $LPATH/mcsys || exit 1
 wget -q https://raw.githubusercontent.com/Argantiu/system-api/main/api/v1/restart.sh -O restart.sh
 wget -q https://raw.githubusercontent.com/Argantiu/system-api/main/api/v1/start.sh -O start.sh
