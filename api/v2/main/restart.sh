02692c07a4b28cffbe80de2012f8d0d5cbe083d4
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-06-21 15:25:11 +0200

    Update restart.sh

--- api/v2/main/restart.sh
+++ api/v2/main/restart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Minecraft Server auto stop script - Do not configure this scipt!!
-# Version 3.0.0.0 made by Argantiu GmBh 06/21/2022 UTC/GMT +1 https://crazycloudcraft.de
+# Version 3.0.0.0-#0 made by Argantiu GmBh 06/21/2022 UTC/GMT +1 https://crazycloudcraft.de
 OPTBASE=
 SERVERBASE=
 MPREFIX=
