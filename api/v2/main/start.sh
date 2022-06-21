42a37a46624d90b92ac0be6e8d59bc60ec3aa2a3
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-06-21 15:58:28 +0200

    Update start.sh

--- api/v2/main/start.sh
+++ api/v2/main/start.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 # Minecraft Server auto stop script - Do not configure this scipt!!
 # Version 3.0.0.0-#0 made by Argantiu GmBh 06/21/2022 UTC/GMT +1 https://crazycloudcraft.de
-#MAINVERSION=
-#PRMCVERSION=3.1.2
-#ASOFTWARE=
+ASOFTWARE=
 OPTBASE=
 SERVERBASE=
 MCNAME=
-#RAM=
-#JAVABIN=
 BEMCSUPPORT=
 BACKUP=
 BPATH=
