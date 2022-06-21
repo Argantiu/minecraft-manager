b9d26a1ec99a1cc1054e814b9d72e48537e18974
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-06-21 16:27:21 +0200

    Update start.sh

--- api/v2/main/start.sh
+++ api/v2/main/start.sh
@@ -8,6 +8,7 @@ MCNAME=
 BEMCSUPPORT=
 BACKUP=
 BPATH=
+MPREFIX=
 # For sed
 MAINVERSION=
 MCNAME=
@@ -16,17 +17,42 @@ RAM=
 JAVABIN=
 #Already Started
 if screen -list | grep -q "$MCNAME"; then
-    echo -e "\033[1;30m[\033[1;32mArgantiu\033[1;30m]\033[0;37m Server has already started! Use screen -r $MCNAME to open it"
+    echo -e "$MPREFIX Server has already started! Use screen -r $MCNAME to open it"
     exit 1
 else
-echo -e "\033[1;30m[\033[1;32mArgantiu\033[1;30m]\033[0;37m Starting server..."
+echo -e "$MPREFIX Starting server..."
 fi
-
 # Path generating
 LPATH=/$OPTBASE/$SERVERBASE
+# Adding if not exist
+if [ -f $LPATH/$MCNAME.jar ]; then
+ echo "Jar exists" >/dev/null 2>&1
+else
+ touch $LPATH/$MCNAME.jar
+fi
+# Auto updater
 
-# Bedrock updater
 
-# Auto updater datas
+# Create backup for your server
+if [[ $BACKUP == "TRUE" ]]; then
+ if [ -f "$MCNAME.jar" ]; then
+    echo -e "\033[1;30m[\033[1;32mArgantiu\033[1;30m]\033[0;37m Create Backup..."
+    echo "Backing up server (to /unused/$BPATH folder)" | /usr/bin/logger -t $MCNAME
+    cd $LPATH/unused/$BPATH && ls -1tr | head -n -10 | xargs -d '\n' rm -f --
+    cd $LPATH || exit 1
+    tar -pzcf ./unused/$BPATH/backup-"$(date +%Y.%m.%d.%H.%M.%S)".tar.gz --exclude='unused/*' ./
+ fi
+fi
+# Bedrock Part
+if [[ $BEMCUPDATE == "TRUE" ]]; then
+ echo -e "$MPREFIX Updateing Bedrock"
+ cd $LPATH/mcsys || exit 1
+ wget -q https://raw.githubusercontent.com/Argantiu/system-api/main/api/v2/be-updater.sh -O be-updater.sh
+ chmod +x be-updater.sh
+ # sed #
+ /bin/bash $LPATH/mcsys/be-updater.sh
+fi
 
-# Startup in software updater
+# Software update and start
+if [[ $ASOFTWARE == "PAPER" ]]; then
+
