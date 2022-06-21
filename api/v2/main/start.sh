67d14b3cf848e6212f18fbb6a3ee64d47790d13b
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-06-21 15:07:31 +0200

    Create start.sh

---
+++ api/v2/main/start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Minecraft Server auto stop script - Do not configure this scipt!!
+# Version 3.0.0.0-#0 made by Argantiu GmBh 06/21/2022 UTC/GMT +1 https://crazycloudcraft.de
+MAINVERSION=
+PRMCVERSION=
+ASOFTWARE=
+OPTBASE=
+SERVERBASE=
+MCNAME=
+RAM=
+JAVABIN=
+BEMCSUPPORT=
+BACKUP=
+BPATH=
+#Already Started
+if screen -list | grep -q "$MCNAME"; then
+    echo -e "\033[1;30m[\033[1;32mArgantiu\033[1;30m]\033[0;37m Server has already started! Use screen -r $MCNAME to open it"
+    exit 1
+else
+echo -e "\033[1;30m[\033[1;32mArgantiu\033[1;30m]\033[0;37m Starting server..."
+fi
+
+# Path generating
+LPATH=/$OPTBASE/$SERVERBASE
