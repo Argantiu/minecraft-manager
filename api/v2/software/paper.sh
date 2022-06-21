7c08d2d5ea8b18d29d3251e837738816dc394353
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-06-21 16:04:11 +0200

    Update paper.sh

--- api/v2/software/paper.sh
+++ api/v2/software/paper.sh
@@ -4,6 +4,8 @@
 MAINVERSION=
 MCNAME=
 LPATH=
+RAM=
+JAVABIN=
 mkdir -p $LPATH/mcsys/jar
 cd $LPATH/mcsys/jar || exit 1
 rm -f version.json
@@ -27,3 +29,8 @@ else
   rm version.json
  fi
 fi
+# Starting server
+cd $LPATH || exit 1
+echo "Starting $LPATH/$MCNAME.jar" | /usr/bin/logger -t $MCNAME
+screen -d -m -L -S $MCNAME  /bin/bash -c "$JAVABIN -Xms$RAM -Xmx$RAM -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar $MCNAME.jar nogui"
+exit 0
