0119eacfdeb8270c453491f9d186ae63061c4121
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-08-18 13:49:31 +0200

    Update stop.sh

--- api/v2/main/en/stop.sh
+++ api/v2/main/en/stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Automatic minecraft server scrpit - Edit at your own risk!!
+# Automatic minecraft server script - Edit at your own risk!!
 # Version 3.0.0.0-#0 created by Argantiu GmBh 2022-08-08 https://crazycloudcraft.de
