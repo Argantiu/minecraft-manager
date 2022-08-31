b3b0a8c20f1fdde1e1171452f0ad7bb753a9aba7
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-08-31 21:01:41 +0200

    Update stop.sh

--- api/v2/main/de/stop.sh
+++ api/v2/main/de/stop.sh
@@ -14,8 +14,8 @@ if ! screen -list | grep -q "$MCNAME"; then
 fi
 
 # Stoppe den Server
-echo -e "$MPREFIX Notification: Stppe $DISPLAYNAME Server ..."
-echo "[Argantiu] Notification: Stppe $DISPLAYNAME Server ..." | /usr/bin/logger -t $MCNAME
+echo -e "$MPREFIX Notification: Stppe $MCNAME Server ..."
+echo "[Argantiu] Notification: Stppe $MCNAME Server ..." | /usr/bin/logger -t $MCNAME
 # Sieht nach, ob Spieler Online sind
 if [[ $ASOFTWARE == "PAPER" ]] || [[ $ASOFTWARE == "SPIGOT" ]] || [[ $ASOFTWARE == "BUKKIT" ]] || [[ $ASOFTWARE == "PURPUR" ]] || [[ $ASOFTWARE == "MOHIST" ]] && [[ $MCOUNT == "TRUE" ]] || [[ $MCOUNT == "true" ]]; then
 cd $MTPATH/mcsys/cache || exit 1
@@ -23,7 +23,7 @@ hostname -I > ip-info.txt
 MCIPAD=$(cat < ip-info.txt | grep -o '^\S*')
 MCPORT=$(cat < $MTPATH/server.properties | grep server-port= | cut -b 13,14,1)
 
-wget -q https://api.minetools.eu/ping/"$MCIPAD"/"$MCPORT" -O online-info
+wget -q https://api.minetools.eu/ping/"$MCIPAD"/"$MCPORT" -O online-info.txt
 if grep -q error "online-info.txt"; then
  echo -e "ERROR the Server information is invalid."
  echo -e "Please open your port to the outside or change the port."
@@ -34,21 +34,21 @@ fi
 
 # Starte Countdown, wenn Spieler Online sind
 if [[ $MCOTYPE = "0" ]]; then
- echo ""
+ echo "0 Spieler Online, Perfekt."
 else
- screen -Rd $MCNAME -X stuff "say Server stoppt in 10 sekunden! $(printf '\r')"
+ screen -Rd $MCNAME -X stuff "say &f Server stoppt in 10 sekunden! $(printf '\r')"
  sleep 6s
- screen -Rd $MCNAME -X stuff "say Server stoppt in 4 $DISPLAYTRANZTIME ! $(printf '\r')"
+ screen -Rd $MCNAME -X stuff "say &f Server stoppt in 4 sekunden! $(printf '\r')"
  sleep 1s
- screen -Rd $MCNAME -X stuff "say Server stoppt in 3 $DISPLAYTRANZTIME ! $(printf '\r')"
+ screen -Rd $MCNAME -X stuff "say &f Server stoppt in 3 sekunden! $(printf '\r')"
  sleep 1s
- screen -Rd $MCNAME -X stuff "say Server stoppt in 2 $DISPLAYTRANZTIME ! $(printf '\r')"
+ screen -Rd $MCNAME -X stuff "say &f Server stoppt in 2 sekunden! $(printf '\r')"
  sleep 1s
- screen -Rd $MCNAME -X stuff "say Server stoppt in 1 $DISPLAYTRANZTIME ! $(printf '\r')"
+ screen -Rd $MCNAME -X stuff "say &f Server stoppt in 1er sekunde! $(printf '\r')"
  sleep 1s
 fi
-screen -Rd $MCNAME -X stuff "say Server stoppt.$(printf '\r')"
-echo "Schalte Minecraft Server aus..." | /usr/bin/logger -t $MCNAME
+screen -Rd $MCNAME -X stuff "say &f Server stoppt.$(printf '\r')"
+echo "Schalte Minecraft $MCNAME Server aus..." | /usr/bin/logger -t $MCNAME
 
 # Wait up to 20 seconds for server to close
 StopChecks=0
@@ -67,5 +67,5 @@ if screen -list | grep -q "$MCNAME"; then
   pkill -15 -f "SCREEN -dmSL $MCNAME"
 fi
 
-echo -e "$MPREFIX Minecraft $SERVERBASE gestoppt."
-echo -e "Minecraft Server $SERVERBASE stoppt." | /usr/bin/logger -t $MCNAME
+echo -e "$MPREFIX Minecraft $MCNAME gestoppt."
+echo -e "Minecraft Server $MCNAME stoppt." | /usr/bin/logger -t $MCNAME
