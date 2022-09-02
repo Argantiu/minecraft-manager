247c486cf71479f6cde82990099655348aeaaacd
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-09-02 08:57:53 +0200

    Update updater.sh

--- api/v2/update/updater.sh
+++ api/v2/update/updater.sh
@@ -2,3 +2,44 @@
 # Updater for the manage system.
 
 Update things and load things, that just were disapered like dependencys
+
+if ! command -v joe &> /dev/null
+  then
+     apt-get install joe -y
+     echo "joe installed"
+ fi
+ if ! command -v screen &> /dev/null
+ then
+     apt-get install screen -y
+     echo "screen installed"
+ fi
+ if ! command -v sudo &> /dev/null
+ then
+     apt-get install sudo -y
+     echo "sudo installed"
+ fi
+ if ! command -v zip &> /dev/null
+ then
+     apt-get install zip -y
+     echo "zip installed"
+ fi
+ if ! command -v wget &> /dev/null
+ then
+     apt-get install wget -y
+     echo "wget installed"
+ fi
+ if ! command -v xargs &> /dev/null
+ then
+     apt-get install findutils -y
+     echo "findutils installed"
+ fi
+ if ! command -v diff &> /dev/null
+ then
+     apt-get install diffutils -y
+     echo "diffutils installed"
+ fi
+ if ! command -v rpl &> /dev/null
+ then
+     apt-get install rpl -y
+     echo "rpl installed"
+ fi
