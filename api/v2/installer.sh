1cf010aed76f8d9418844449797dadd18f9d5781
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-09-11 13:19:44 +0200

    Update installer.sh

--- api/v2/installer.sh
+++ api/v2/installer.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 # installer
+
+File locations:
+x = will installed though other progress
+... /MainServer/manage.tool
+... /MainServer/mcsys/start.sh
+... /MainServer/mcsys/stop.sh
+... /MainServer/mcsys/restart.sh
+... /MainServer/mcsys/configs/mcsys.config
+... /MainServer/mcsys/updater/*
+x ... /MainServer/mcsys/backsave/jar/*
+x ... /MainServer/mcsys/backsave/geyser/*
+x ... /MainServer/mcsys/backsave/floodgate/*
+x ... /MainServer/mcsys/software/*
