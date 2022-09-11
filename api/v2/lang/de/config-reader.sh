521b21a5cd190df59e9f78bed135a713339dbb73
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-09-11 13:08:10 +0200

    Delete config-reader.sh

--- api/v2/lang/de/config-reader.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Read all settings to get a computer output
-#cat mcsys.config | grep [pattern] | awk '{ print $number-of-column }' >> file1
-cat mcsys.config | grep MAINVERSION= | sed -i "0,/MAINVERSION=.*/s//MAINVERSION=$MAINVERSION/" $LPATH/mcsys/start.sh >/dev/null 2>&1
