f9555f22e5f00bdd57f8ae938a48c81f63a01ae8
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-09-12 11:24:06 +0200

    Update paper.sh

--- api/v2/software/paper.sh
+++ api/v2/software/paper.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Minecraft Server auto stop script - Do not configure this scipt!!
 # Version 3.0.0.0-#0 made by Argantiu GmBh 06/21/2022 UTC/GMT +1 https://crazycloudcraft.de
-MAINVERSION=
-MCNAME=
-LPATH=
-RAM=
-JAVABIN=
+MAINVERSION=$(cat ./../configs/mcsys.config | grep MAINVERSION= | cut -d '=' -f2)
+MCNAME=$(cat ./configs/mcsys.config | grep MCNAME= | cut -d '=' -f2)
+MTPATH=$(cat ./configs/mcsys.config | grep MTPATH= | cut -d '=' -f2)
+RAM=$(cat ./configs/mcsys.config | grep RAM= | cut -d '=' -f2)
+JAVABIN=$(cat ./configs/mcsys.config | grep JAVABIN= | cut -d '=' -f2)
 mkdir -p $LPATH/mcsys/jar
-cd $LPATH/mcsys/jar || exit 1
+cd $MTPATH/mcsys/jar || exit 1
 rm -f version.json
 wget -q https://api.papermc.io/v2/projects/paper/versions/$MAINVERSION/ -O version.json
 LATEST=$(cat < version.json | jq -r ".builds" | grep -v "," | grep -e "[0-9]" | tr -d " ")
@@ -16,11 +16,11 @@ unzip -qq -t paper-$MAINVERSION-$LATEST.jar
 if [ "$?" -ne 0 ]; then
  echo "Downloaded paper-$MAINVERSION-$LATEST.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
 else
- diff -q paper-$MAINVERSION-$LATEST.jar $LPATH/$MCNAME.jar >/dev/null 2>&1
+ diff -q paper-$MAINVERSION-$LATEST.jar $MTPATH/$MCNAME.jar >/dev/null 2>&1
  if [ "$?" -eq 1 ]; then
   cp paper-$MAINVERSION-$LATEST.jar paper-$MAINVERSION-$LATEST.jar."$(date +%Y.%m.%d.%H.%M.%S)"
-  mv paper-$MAINVERSION-$LATEST.jar $LPATH/$MCNAME.jar
-  /usr/bin/find $LPATH/mcsys/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+  mv paper-$MAINVERSION-$LATEST.jar $MTPATH/$MCNAME.jar
+  /usr/bin/find $MTPATH/mcsys/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
   echo "paper-$MAINVERSION-$LATEST has been updated" | /usr/bin/logger -t $MCNAME
   rm version.json
  else
@@ -31,6 +31,6 @@ else
 fi
 # Starting server
 cd $LPATH || exit 1
-echo "Starting $LPATH/$MCNAME.jar" | /usr/bin/logger -t $MCNAME
+echo "Starting $MTPATH/$MCNAME.jar" | /usr/bin/logger -t $MCNAME
 screen -d -m -L -S $MCNAME  /bin/bash -c "$JAVABIN -Xms$RAM -Xmx$RAM -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar $MCNAME.jar nogui"
 exit 1
