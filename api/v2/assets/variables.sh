4511d0bf657893b82e506443b6283043e710e40f
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-10-17 19:21:59 +0200

    Update variables.sh

--- api/v2/assets/variables.sh
+++ api/v2/assets/variables.sh
@@ -2,16 +2,18 @@
 # Here are the System variables don't edit them here. You can use the mcsys.yml ;)
 # Automatic minecraft server script - Edit at your own risks!!
 # Version 3.0.0.0-#0 created by CrazyCloudCraft https://crazycloudcraft.de
-MCONF=./configs/mcsys.yml
-MLANG=./configs/messages.lang
-ASOFTWARE=$(cat < $MCONF | grep software: | cut -d ':' -d ' ' -f2)
-MTPATH=$(cat < $MCONF | grep server.directory: | cut -d ':' -d ' ' -f2)
-BEUPDATE=$(cat < $MCONF | grep BEMCUPDATE= | cut -d ':' -d ' ' -f2)
-BACKUP=$(cat < $MCONF | grep BACKUP= | cut -d ':' -d ' ' -f2)
-BETTERBACKUP=$(cat < $MCONF | grep BETTERBACKUP= | cut -d ':' -d ' ' -f2)
-MPREFIX=$(cat < $MCONF | grep MPREFIX= | cut -d ':' -d ' ' -f2)
-MCNAME=$(cat < $MCONF | grep MCNAME= | cut -d ':' -d ' ' -f2)
+MLANG=./messages.lang
+# start.sh
+ASOFTWARE=$(cat < mcsys.yml | grep software: | cut -d ':' -d ' ' -f2)
+MTPATH=$(cat < mcsys.yml | grep server.directory: | cut -d ':' -d ' ' -f2)
+BEUPDATE=$(cat < mcsys.yml | grep BEMCUPDATE= | cut -d ':' -d ' ' -f2)
+BACKUP=$(cat < mcsys.yml | grep BACKUP= | cut -d ':' -d ' ' -f2)
+BETTERBACKUP=$(cat < mcsys.yml | grep BETTERBACKUP= | cut -d ':' -d ' ' -f2)
+MPREFIX=$(cat < mcsys.yml | grep MPREFIX= | cut -d ':' -d ' ' -f2)
+MCNAME=$(cat < mcsys.yml | grep MCNAME= | cut -d ':' -d ' ' -f2)
 OPTBASE=
 SERVERBASE=
+# manage.tool
+SERVERNAME=$(cat < mcsys.yml | grep MCNAME: | cut -d '=' -f2)
 # Tranzlations
 TR1=$(cat < $MLANG | grep startsh.already.online: | cut -d ':' -d ' ' -f2)
