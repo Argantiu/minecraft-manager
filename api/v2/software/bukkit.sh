36461d2b2147d4e189e9b721a604492db2b5d9d8
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-10-26 18:18:07 +0200

    Update bukkit.sh

--- api/v2/software/bukkit.sh
+++ api/v2/software/bukkit.sh
@@ -13,46 +13,23 @@ if [ "$?" -ne 0 ]; then
 else
  diff -q BuildTools-1.jar BuildTools.jar >/dev/null 2>&1
  if [ "$?" -eq 1 ]; then 
- rm BuildTools.jar && mv BuildTools-1.jar BuildTools.jar && mkdir -p $MTPATH/mcsys/saves/jar/cache
- cd $MTPATH/mcsys/saves/jar/cache || exit 1
- cp ./../BuildTools.jar .
- java -jar BuildTools.jar --rev $MAINVERSION
- cp ./CraftBukkit/target/craftbukkit-*.jar $MTPATH/mcsys/saves/jar/craftbukkit-$MAINVERSION.jar"$(date +%Y.%m.%d.%H.%M.%S)"
- mv 
- 
- 
- 
- 
- 
-mkdir -p $LPATH/mcsys/build
-mkdir -p $LPATH/mcsys/buktool
-mkdir -p $LPATH/mcsys/
-cd $LPATH/mcsys/build/mcmain || exit 1
-wget -q https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar -O BuildTools.jar
-unzip -qq -t BuildTools.jar
-if [ "$?" -ne 0 ]; then
- echo "Downloaded BuildTools.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
-else
- diff $LPATH/mcsys/build/mcmain/BuildTools.jar $LPATH/mcsys/buktool/BuildTools.jar >/dev/null 2>&1
- if [ "$?" -eq 1 ]; then
-  cp $LPATH/mcsys/build/mcmain/BuildTools.jar $LPATH/mcsys/buktool/BuildTools.jar
-  cd $LPATH/mcsys/buktool || exit 1
-  cp BuildTools.jar BuildTools.jar"$(date +%Y.%m.%d.%H.%M.%S)"
-  cd $LPATH/mcsys/build/mcmain || exit 1
-  
-  cp $LPATH/mcsys/build/mcmain/CraftBukkit/target/craftbukkit-*.jar $LPATH/mcsys/buktool/craftbukkit-
-  mv $LPATH/mcsys/build/mcmain/CraftBukkit/target/craftbukkit-*.jar $LPATH/$MCNAME.jar
-  rm -r $LPATH/mcsys/build/mcmain
-  /usr/bin/find $LPATH/mcsys/build/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
-  /usr/bin/find $LPATH/mcsys/buktool/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+  rm BuildTools.jar && mv BuildTools-1.jar BuildTools.jar && mkdir -p $MTPATH/mcsys/saves/jar/cache
+  cd $MTPATH/mcsys/saves/jar/cache || exit 1
+  cp ./../BuildTools.jar .
+  java -jar BuildTools.jar --rev $MAINVERSION
+  cp ./CraftBukkit/target/craftbukkit-*.jar $MTPATH/mcsys/saves/jar/craftbukkit-$MAINVERSION.jar"$(date +%Y.%m.%d.%H.%M.%S)"
+  mv ./CraftBukkit/target/craftbukkit-*.jar $MTPATH/$MCNAME.jar
+  cd ../ || exit 1
+  rm -r ./cache
+  /usr/bin/find $MTPATH/mcsys/saves/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
   echo "bukkit-$MAINVERSION.jar has been updated" | /usr/bin/logger -t $MCNAME
  else
   echo "No BuildTools.jar update neccessary" | /usr/bin/logger -t $MCNAME
-  rm BuildTools.jar
+  rm BuildTools-1.jar
  fi
 fi
 #Starting server
-cd $LPATH || exit 1
-echo "Starting $LPATH/$MCNAME.jar" | /usr/bin/logger -t $MCNAME
+cd $MTPATH || exit 1
+echo "Starting $MTPATH/$MCNAME.jar" | /usr/bin/logger -t $MCNAME
 screen -d -m -L -S $MCNAME  /bin/bash -c "$JAVABIN -Xms$RAM -Xmx$RAM -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar $MCNAME.jar nogui"
 exit 1
