f92e46bc23c139e20087f2cc64ffe9c60a9ef11f
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-10-26 21:09:32 +0200

    Update geyser.sh

--- api/v2/software/be/geyser.sh
+++ api/v2/software/be/geyser.sh
@@ -3,37 +3,32 @@
 # Version 3.0.0.0-#0 made by Argantiu GmBh 06/21/2022 UTC/GMT +1 https://crazycloudcraft.de
 # shellcheck source=/dev/null
 . ./../configs/variables.sh
-# Floodgate for normal servers
-if [[ $ASOFTWARE == "PAPER" ]] || [[ $ASOFTWARE == "SPIGOT" ]] || [[ $ASOFTWARE == "BUKKIT" ]] || [[ $ASOFTWARE == "PURPUR" ]] && [[ $BESUPPORT == "TRUE" ]]; then
- cd $LPATH/plugins || exit 1
- mkdir -p $LPATH/mcsys/floodgate
- cd $LPATH/mcsys/floodgate || exit 1
+# Floodgate
+mkdir -p $MTPATH/mcsys/floodgate
+if [ ! $ASOFTWARE = "modded/mohist.sh" ]; then
+ if [ ! -f $MTPATH/plugins/floodgate-spigot.jar ]; then touch $MTPATH/plugins/floodgate-spigot.jar
+ fi
+ cd $MTPATH/mcsys/floodgate || exit 1
  wget -q https://ci.opencollab.dev/job/GeyserMC/job/Floodgate/job/master/lastSuccessfulBuild/artifact/spigot/target/floodgate-spigot.jar -O floodgate-spigot.jar
  unzip -qq -t floodgate-spigot.jar
  if [ "$?" -ne 0 ]; then
   echo "Downloaded floodgate default is corrupt. No update." | /usr/bin/logger -t $MCNAME
  else
-  if [ -f $LPATH/plugins/floodgate-spigot.jar ]; then
-   echo "Floodgate plugin exists" | /usr/bin/logger -t $MCNAME
-  else
-   touch $LPATH/plugins/floodgate-spigot.jar
-  fi
-  diff -q floodgate-spigot.jar $LPATH/plugins/floodgate-spigot.jar >/dev/null 2>&1
+  diff -q floodgate-spigot.jar $MTPATH/plugins/floodgate-spigot.jar >/dev/null 2>&1
   if [ "$?" -eq 1 ]; then
    cp floodgate-spigot.jar floodgate-spigot.jar."$(date +%Y.%m.%d.%H.%M.%S)"
-   mv floodgate-spigot.jar $LPATH/plugins/floodgate-spigot.jar
-   /usr/bin/find $LPATH/mcsys/floodgate/* -type f -mtime +6 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+   mv floodgate-spigot.jar $MTPATH/plugins/floodgate-spigot.jar
+   /usr/bin/find $MTPATH/mcsys/saves/floodgate/* -type f -mtime +5 -delete 2>&1 | /usr/bin/logger -t $MCNAME
    echo "floodgate default has been updated" | /usr/bin/logger -t $MCNAME
   else
    echo "No floodgate default update neccessary" | /usr/bin/logger -t $MCNAME
    rm floodgate-spigot.jar
   fi
  fi
+else echo -e "Bedrock support doesn't work on this software! Please use an other sofware or disable Bedrock support."
 fi
 
-# Geyser Updater for nomal servers
-if [[ $ASOFTWARE == "PAPER" ]] || [[ $ASOFTWARE == "SPIGOT" ]] || [[ $ASOFTWARE == "BUKKIT" ]] || [[ $ASOFTWARE == "PURPUR" ]] && [[ $GBESUPPORT == "TRUE" ]]; then
- cd $LPATH/plugins || exit 1
+# Geyser
  mkdir -p $LPATH/mcsys/geyser
  cd $LPATH/mcsys/geyser || exit 1
  wget -q https://ci.opencollab.dev/job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/target/Geyser-Spigot.jar -O Geyser-Spigot.jar
@@ -58,80 +53,3 @@ if [[ $ASOFTWARE == "PAPER" ]] || [[ $ASOFTWARE == "SPIGOT" ]] || [[ $ASOFTWARE 
   fi
  fi
 fi
-
-# Floodgate for Proxy
-if [[ $ASOFTWARE == "BUNGEECORD" ]] || [[ $ASOFTWARE == "VELOCITY" ]] || [[ $ASOFTWARE == "WATERFALL" ]] && [[ $BESUPPORT == "TRUE" ]]; then
- cd $LPATH/plugins || exit 1
- mkdir -p $LPATH/mcsys/floodgate
- cd $LPATH/mcsys/floodgate || exit 1
- if [ $ASOFTWARE = "BUNGEECORD" ] || [ $ASOFTWARE = "WATERFALL" ]; then
- wget -q https://ci.opencollab.dev/job/GeyserMC/job/Floodgate/job/master/lastSuccessfulBuild/artifact/bungee/target/floodgate-bungee.jar -O floodgate-bungee.jar
- unzip -qq -t floodgate-bungee.jar
- if [ "$?" -ne 0 ]; then
-  echo "Downloaded floodgate for Bungeecord and Waterfall is corrupt. No update." | /usr/bin/logger -t $MCNAME
- else
-  if [ -f $LPATH/plugins/floodgate-bungee.jar ]; then
-   echo "Geyser bungee plugin exists" | /usr/bin/logger -t $MCNAME
-  else
-   touch $LPATH/plugins/floodgate-bungee.jar
-  fi
-  diff -q floodgate-bungee.jar $LPATH/plugins/floodgate-bungee.jar >/dev/null 2>&1
-  if [ "$?" -eq 1 ]; then
-   cp floodgate-bungee.jar floodgate-bungee.jar."$(date +%Y.%m.%d.%H.%M.%S)"
-   mv floodgate-bungee.jar $LPATH/plugins/floodgate-bungee.jar
-   /usr/bin/find $LPATH/mcsys/floodgate/* -type f -mtime +6 -delete 2>&1 | /usr/bin/logger -t $MCNAME
-   echo "floodgate for Bungeecord and Waterfall has been updated" | /usr/bin/logger -t $MCNAME
-  else
-   echo "No floodgate bungee update neccessary" | /usr/bin/logger -t $MCNAME
-   rm floodgate-bungee.jar
-  fi
- fi
- fi
-# Velocity part
- if [[ $ASOFTWARE == "VELOCITY" ]]; then
- wget -q https://ci.opencollab.dev/job/GeyserMC/job/Floodgate/job/master/lastSuccessfulBuild/artifact/velocity/target/floodgate-velocity.jar -O floodgate-velocity.jar
- unzip -qq -t floodgate-velocity.jar
- if [ "$?" -ne 0 ]; then
-  echo "Downloaded floodgate for velocity is corrupt. No update." | /usr/bin/logger -t $MCNAME
- else
-  if [ -f $LPATH/plugins/floodgate-velocity.jar ]; then
-   echo "floodgate-velocity.jar plugin exists" | /usr/bin/logger -t $MCNAME
-  else
-   touch $LPATH/plugins/floodgate-velocity.jar
-  fi
-  diff -q floodgate-velocity.jar $LPATH/plugins/floodgate-velocity.jar >/dev/null 2>&1
-  if [ "$?" -eq 1 ]; then
-   cp floodgate-velocity.jar floodgate-velocity.jar."$(date +%Y.%m.%d.%H.%M.%S)"
-   mv floodgate-velocity.jar $LPATH/plugins/floodgate-velocity.jar
-   /usr/bin/find $LPATH/mcsys/floodgate/* -type f -mtime +6 -delete 2>&1 | /usr/bin/logger -t $MCNAME
-   echo "floodgate for velocity has been updated" | /usr/bin/logger -t $MCNAME
-  else
-   echo "No floodgate velocity update neccessary" | /usr/bin/logger -t $MCNAME
-   rm floodgate-velocity.jar
-  fi
- fi
- fi
-fi
-
-# Geyser for Proxy
-if [[ $ASOFTWARE == "BUNGEECORD" ]] || [[ $ASOFTWARE == "VELOCITY" ]] || [[ $ASOFTWARE == "WATERFALL" ]] && [[ $BESUPPORT == "TRUE" ]]; then
- cd $LPATH/plugins || exit 1
- mkdir -p $LPATH/mcsys/geyser
- cd $LPATH/mcsys/geyser || exit 1
- if [ $ASOFTWARE = "BUNGEECORD" ] || [ $ASOFTWARE = "WATERFALL" ]; then
- wget -q https://ci.opencollab.dev/job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/bungeecord/target/Geyser-BungeeCord.jar -O Geyser-BungeeCord.jar
- unzip -qq -t Geyser-BungeeCord.jar
- if [ "$?" -ne 0 ]; then
-  echo "Downloaded Geyser Bungeecord and Waterfall is corrupt. No update." | /usr/bin/logger -t $MCNAME
- else
-  if [ -f $LPATH/plugins/Geyser-BungeeCord.jar ]; then
-   echo "Geyser-BungeeCord.jar plugin exists" | /usr/bin/logger -t $MCNAME
-  else
-   touch $LPATH/plugins/Geyser-BungeeCord.jar
-
-# Error for Mod Servers
-if [[ $ASOFTWARE == "FORGE" ]] || [[ $ASOFTWARE == "MOHIST" ]] || [[ $ASOFTWARE == "FABRIC" ]] || [[ $ASOFTWARE == "MINECRAFT" ]] && [[ $BESUPPORT == "TRUE" ]]; then
-echo -e "Bedrock support doesn't work on this software! Please use an other sofware or disable Bedrock support."
-fi
-
-exit 1
