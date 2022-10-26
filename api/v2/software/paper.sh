1151a5cceeba2f6adfe00c95f44295edf0aff1f0
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-10-26 17:21:45 +0200

    Update paper.sh

--- api/v2/software/paper.sh
+++ api/v2/software/paper.sh
@@ -9,18 +9,14 @@ wget -q https://api.papermc.io/v2/projects/paper/versions/"$MAINVERSION"/ -O ver
 LATEST=$(cat < version.json | jq -r ".builds" | grep -v "," | grep -e "[0-9]" | tr -d " ")
 wget -q https://api.papermc.io/v2/projects/paper/versions/"$MAINVERSION"/builds/"$LATEST"/downloads/paper-"$MAINVERSION"-"$LATEST".jar -O paper-$MAINVERSION-$LATEST.jar
 unzip -qq -t paper-$MAINVERSION-$LATEST.jar
-if [ "$?" -ne 0 ]; then
- echo "Downloaded paper-$MAINVERSION-$LATEST.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
-else
- diff -q paper-$MAINVERSION-$LATEST.jar $MTPATH/$MCNAME.jar >/dev/null 2>&1
+if [ "$?" -ne 0 ]; then echo "Downloaded paper-$MAINVERSION-$LATEST.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
+else diff -q paper-$MAINVERSION-$LATEST.jar $MTPATH/$MCNAME.jar >/dev/null 2>&1
  if [ "$?" -eq 1 ]; then
-  cp paper-$MAINVERSION-$LATEST.jar paper-$MAINVERSION-$LATEST.jar."$(date +%Y.%m.%d.%H.%M.%S)"
-  mv paper-$MAINVERSION-$LATEST.jar $MTPATH/$MCNAME.jar
+  cp paper-$MAINVERSION-$LATEST.jar paper-$MAINVERSION-$LATEST.jar."$(date +%Y.%m.%d.%H.%M.%S)" && mv paper-$MAINVERSION-$LATEST.jar $MTPATH/$MCNAME.jar
   /usr/bin/find $MTPATH/mcsys/saves/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
   echo "paper-$MAINVERSION-$LATEST has been updated" | /usr/bin/logger -t $MCNAME
   rm -f version.json
- else
-  echo "No paper-$MAINVERSION-$LATEST update neccessary" | /usr/bin/logger -t $MCNAME
+ else echo "No paper-$MAINVERSION-$LATEST update neccessary" | /usr/bin/logger -t $MCNAME
   rm paper-$MAINVERSION-$LATEST.jar
   rm -f version.json
  fi
