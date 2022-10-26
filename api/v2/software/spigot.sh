9265c84f452bcc50cef5e1b840bbff8981f37d93
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-10-26 18:21:13 +0200

    Update spigot.sh

--- api/v2/software/spigot.sh
+++ api/v2/software/spigot.sh
@@ -1,46 +1,34 @@
 #!/bin/bash
 # Minecraft Server auto stop script - Do not configure this scipt!!
-# Version 3.0.0.0-#0 made by Argantiu GmBh 06/21/2022 UTC/GMT +1 https://crazycloudcraft.de
-MAINVERSION=
-MCNAME=
-LPATH=
-RAM=
-JAVABIN=
-#Spigot / Bukkit: Getting Update form your selected version.
-mkdir -p $LPATH/mcsys/build
-mkdir -p $LPATH/mcsys/spitool
-mkdir -p $LPATH/mcsys/build/mcmain
-cd $LPATH/mcsys/build/mcmain || exit 1
-wget -q https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar -O BuildTools.jar
-unzip -qq -t BuildTools.jar
+# Version 3.0.0.0-#0 made by CrazyCloudCraft https://crazycloudcraft.de
+. ./../configs/variables.sh
+cd $MTPATH/mcsys/saves/jar || exit 1
+if [ ! -f $MTPATH/mcsys/saves/jar/BuildTools.jar ]; then touch $MTPATH/mcsys/saves/jar/BuildTools.jar
+fi
+wget -q https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar -O BuildTools-1.jar
+unzip -qq -t BuildTools-1.jar
 if [ "$?" -ne 0 ]; then
  echo "Downloaded BuildTools.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
 else
- if [ -f $LPATH/mcsys/spitool/BuildTools.jar ]; then
-  echo "BuildTools exists" | /usr/bin/logger -t $MCNAME
- else
-  touch $LPATH/mcsys/spitool/BuildTools.jar
- fi
- diff $LPATH/mcsys/build/mcmain/BuildTools.jar $LPATH/mcsys/spitool/BuildTools.jar >/dev/null 2>&1
- if [ "$?" -eq 1 ]; then
-  cp $LPATH/mcsys/build/mcmain/BuildTools.jar $LPATH/mcsys/spitool/BuildTools.jar
-  cd $LPATH/mcsys/spitool || exit 1
-  cp BuildTools.jar BuildTools.jar"$(date +%Y.%m.%d.%H.%M.%S)"
-  cd $LPATH/mcsys/build/mcmain || exit 1
+ diff -q BuildTools-1.jar BuildTools.jar >/dev/null 2>&1
+ if [ "$?" -eq 1 ]; then 
+  rm BuildTools.jar && mv BuildTools-1.jar BuildTools.jar && mkdir -p $MTPATH/mcsys/saves/jar/cache
+  cd $MTPATH/mcsys/saves/jar/cache || exit 1
+  cp ./../BuildTools.jar .
   java -jar BuildTools.jar --rev $MAINVERSION
-  cp $LPATH/mcsys/build/mcmain/spigot-$MAINVERSION.jar $LPATH/mcsys/spitool/spigot-$MAINVERSION.jar"$(date +%Y.%m.%d.%H.%M.%S)"
-  mv $LPATH/mcsys/build/mcmain/spigot-$MAINVERSION.jar $LPATH/$MCNAME.jar
-  rm -r $LPATH/mcsys/build/mcmain
-  /usr/bin/find $LPATH/mcsys/build/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
-  /usr/bin/find $LPATH/mcsys/spitool/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
-  echo "spigot/bukkit-$MAINVERSION.jar has been updated" | /usr/bin/logger -t $MCNAME
+  cp ./CraftBukkit/target/spigot-*.jar $MTPATH/mcsys/saves/jar/spigot-$MAINVERSION.jar"$(date +%Y.%m.%d.%H.%M.%S)"
+  mv ./CraftBukkit/target/spigot-*.jar $MTPATH/$MCNAME.jar
+  cd ../ || exit 1
+  rm -r ./cache
+  /usr/bin/find $MTPATH/mcsys/saves/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+  echo "spigot-$MAINVERSION.jar has been updated" | /usr/bin/logger -t $MCNAME
  else
   echo "No BuildTools.jar update neccessary" | /usr/bin/logger -t $MCNAME
-  rm BuildTools.jar
+  rm BuildTools-1.jar
  fi
 fi
 # Starting server
-cd $LPATH || exit 1
-echo "Starting $LPATH/$MCNAME.jar" | /usr/bin/logger -t $MCNAME
+cd $MTPATH || exit 1
+echo "Starting $MTPATH/$MCNAME.jar" | /usr/bin/logger -t $MCNAME
 screen -d -m -L -S $MCNAME  /bin/bash -c "$JAVABIN -Xms$RAM -Xmx$RAM -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar $MCNAME.jar nogui"
 exit 1
