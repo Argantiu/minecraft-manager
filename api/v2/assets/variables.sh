a4135bf11d6a407bdcd9dfcd1cac277ed2c1cfa7
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-10-26 13:35:36 +0200

    Update variables.sh

--- api/v2/assets/variables.sh
+++ api/v2/assets/variables.sh
@@ -31,8 +31,20 @@ MTPATH=$(cat < mcsys.yml | grep "server.directory:" | cut -d ':' -f2 | tr -d " "
 SERVERBASE=$(cat < mcsys.yml | grep "server.directory:" | rev | cut -d '/' -f1 | rev )
 OPTBASE=$(cat < mcsys.yml | grep "server.directory:" | tr -d " " | cut -d ':' -f2 | sed s/$SERVERBASE//m)
 # Prefix
-COLOR=\033[0;
-MPREFIX=$(cat < mcsys.yml | grep "sys.prefix:" | cut -d ':' -f2 | sed s:§0:\033[0;30m:m | )
+#COLOR=\033[0;
+#MPREFIX=$(cat < mcsys.yml | grep "sys.prefix:" | cut -d ':' -f2 | sed s:§0:\033[0;30m:m | )
+# Farbliste:
+
+# Black:        \033[0;30m'     Dark Gray:     \033[1;30m'
+# Red:          \033[0;31m'     Light Red:     \033[1;31m'
+# Green:        \033[0;32m'     Light Green:   \033[1;32m'
+# Brown/Orange: \033[0;33m'     Yellow:        \033[1;33m'
+# Blue:         \033[0;34m'     Light Blue:    \033[1;34m'
+# Purple:       \033[0;35m'     Light Purple:  \033[1;35m'
+# Cyan:         \033[0;36m'     Light Cyan:    \033[1;36m'
+# Light Gray:   \033[0;37m'     White:         \033[1;37m'
+
+MPREFIX=§8[§aArgantiu§8]
 # Version
 MAINVERSION=$(cat < mcsys.yml | grep "version:" | cut -d ':' -f2)
 # Systemname
