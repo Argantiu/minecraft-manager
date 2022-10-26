75060ed718e2fd6465ac73930252fde33daaa37e
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-10-26 17:49:57 +0200

    Update waterfall.sh

--- api/v2/software/proxy/waterfall.sh
+++ api/v2/software/proxy/waterfall.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 # Minecraft Server auto stop script - Do not configure this scipt!!
 # Version 3.0.0.0-#0 made by Argantiu GmBh 06/21/2022 UTC/GMT +1 https://crazycloudcraft.de
-MAINVERSION=
-MCNAME=
-LPATH=
-RAM=
-JAVABIN=
-#Waterfall: Getting Update form your selected version.
-mkdir -p $LPATH/mcsys/jar
-cd $LPATH/mcsys/jar || exit 1
+. ./../configs/variables.sh
+cd $MTPATH/mcsys/saves/jar || exit 1
 rm -f version.json
 wget -q https://api.papermc.io/v2/projects/waterfall/versions/$MAINVERSION -O version.json
 LATEST=$(cat < version.json | jq -r ".builds" | grep -v "," | grep -e "[0-9]" | tr -d " ")
@@ -17,11 +11,11 @@ unzip -qq -t waterfall-$MAINVERSION-$LATEST.jar
 if [ "$?" -ne 0 ]; then
  echo "Downloaded waterfall-$MAINVERSION-$LATEST.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
 else
- diff -q waterfall-$MAINVERSION-$LATEST.jar $LPATH/$MCNAME.jar >/dev/null 2>&1
+ diff -q waterfall-$MAINVERSION-$LATEST.jar $MTPATH/$MCNAME.jar >/dev/null 2>&1
  if [ "$?" -eq 1 ]; then
   cp waterfall-$MAINVERSION-$LATEST.jar waterfall-$MAINVERSION-$LATEST.jar."$(date +%Y.%m.%d.%H.%M.%S)"
-  mv waterfall-$MAINVERSION-$LATEST.jar $LPATH/$MCNAME.jar
-  /usr/bin/find $LPATH/mcsys/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
+  mv waterfall-$MAINVERSION-$LATEST.jar $MTPATH/$MCNAME.jar
+  /usr/bin/find $MTPATH/mcsys/saves/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
   echo "waterfall-$MAINVERSION-$LATEST has been updated" | /usr/bin/logger -t $MCNAME
   rm version.json
  else
@@ -31,7 +25,7 @@ else
  fi
 fi
 # Starting server
-cd $LPATH || exit 1
-echo "Starting $LPATH/$MCNAME.jar" | /usr/bin/logger -t $MCNAME
+cd $MTPATH || exit 1
+echo "Starting $MTPATH/$MCNAME.jar" | /usr/bin/logger -t $MCNAME
 screen -d -m -L -S $MCNAME  /bin/bash -c "$JAVABIN -Xms$RAM -Xmx$RAM -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar $MCNAME.jar"
 exit 1
