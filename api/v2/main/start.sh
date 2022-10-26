12b0c86449c9f85d499191f54d402057aa17b554
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-10-26 18:44:16 +0200

    Update start.sh

--- api/v2/main/start.sh
+++ api/v2/main/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Automatic minecraft server script - Edit at your own risks!!
 # Version 3.0.0.0-#0 created by CrazyCloudCraft https://crazycloudcraft.de
-# shellcheck source=./../assets/variables.sh
+# shellcheck source=/dev/null
 . ./configs/variables.sh
 # Already Started
 if screen -list | grep -q "$MCNAME"; then echo -e "$MSTART1" && exit 1
