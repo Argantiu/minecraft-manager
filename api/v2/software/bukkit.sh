73db6316fe4a9d517c2098d1446d1e3f86604cb5
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-10-26 18:46:10 +0200

    Update bukkit.sh

--- api/v2/software/bukkit.sh
+++ api/v2/software/bukkit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Minecraft Server auto stop script - Do not configure this scipt!!
 # Version 3.0.0.0-#0 made by CrazyCloudCraft https://crazycloudcraft.de
+# shellcheck source=/dev/null
 . ./../configs/variables.sh
 cd $MTPATH/mcsys/saves/jar || exit 1
 if [ ! -f $MTPATH/mcsys/saves/jar/BuildTools.jar ]; then touch $MTPATH/mcsys/saves/jar/BuildTools.jar
