165663e8b9e587f049fe8ebde63499f95f930f40
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-11-02 16:54:24 +0100

    Update purpur.sh

--- api/v2/software/purpur.sh
+++ api/v2/software/purpur.sh
@@ -2,7 +2,9 @@
 # Minecraft Server auto stop script - Do not configure this script!!
 # Version 3.0.0.0-#0 made by CrazyCloudCraft https://crazycloudcraft.de
 # shellcheck source=/dev/null
-source /../configs/variables.sh
+cd ./../configs || exit 1
+source variables.sh
+cd $MTPATH || exit 1
 mkdir -p $MTPATH/mcsys/saves/jar
 cd $MTPATH/mcsys/saves/jar || exit 1
 rm -f version.json
