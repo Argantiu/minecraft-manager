d7a737fb413e55120e0eb2e6e80e3a979aa61773
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-11-02 18:03:51 +0100

    Update updater.sh

--- api/v2/update/updater.sh
+++ api/v2/update/updater.sh
@@ -1,47 +1,32 @@
 #!/bin/bash
-# Updater for the manage system.
+# Updater for the manage system. Created by CrazyCloudCraft.
 # Version: 3.0.0.0
-
 apt-get upgrade
 apt-get update
-if ! command -v joe &> /dev/null
-  then
-     apt-get install joe -y
-     echo "joe installed"
- fi
- if ! command -v screen &> /dev/null
- then
-     apt-get install screen -y
-     echo "screen installed"
- fi
- if ! command -v sudo &> /dev/null
- then
-     apt-get install sudo -y
-     echo "sudo installed"
- fi
- if ! command -v zip &> /dev/null
- then
-     apt-get install zip -y
-     echo "zip installed"
- fi
- if ! command -v wget &> /dev/null
- then
-     apt-get install wget -y
-     echo "wget installed"
- fi
- if ! command -v xargs &> /dev/null
- then
-     apt-get install findutils -y
-     echo "findutils installed"
- fi
- if ! command -v diff &> /dev/null
- then
-     apt-get install diffutils -y
-     echo "diffutils installed"
- fi
- if ! command -v rpl &> /dev/null
- then
-     apt-get install rpl -y
-     echo "rpl installed"
- fi
-
+if ! command -v joe &> /dev/null; then apt-get install joe -y &> /dev/null
+fi
+if ! command -v screen &> /dev/null; then apt-get install screen -y &> /dev/null
+fi
+if ! command -v sudo &> /dev/null; then apt-get install sudo -y &> /dev/null
+fi
+if ! command -v zip &> /dev/null; then apt-get install zip -y &> /dev/null
+fi
+if ! command -v xargs &> /dev/null; then apt-get install findutils -y &> /dev/null
+fi
+if ! command -v diff &> /dev/null; then apt-get install diffutils -y &> /dev/null
+fi
+if ! command -v rpl &> /dev/null; then apt-get install rpl -y &> /dev/null
+fi
+cd ./../configs || exit 1
+rm variables.sh
+wget -q https://raw.githubusercontent.com/$IFCREATEDFORK/main/api/v2/assets/variables.sh
+chmod +x variables.sh
+cd ./../ || exit 1
+wget -q https://raw.githubusercontent.com/$IFCREATEDFORK/main/api/v2/main/restart.sh
+wget -q https://raw.githubusercontent.com/$IFCREATEDFORK/main/api/v2/main/start.sh
+wget -q https://raw.githubusercontent.com/$IFCREATEDFORK/main/api/v2/main/stop.sh
+chmod +x ./*.sh
+cd ./../../ || exit 1
+wget -q https://raw.githubusercontent.com/$IFCREATEDFORK/main/api/v2/assets/manage.tool
+chmod +x manage.tool
+exit 0
