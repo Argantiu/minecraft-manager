177fdd5f56822d6130d1bf674a049f69de963f8e
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-11-09 09:13:39 +0100

    Update geyser-proxy.sh

--- api/v2/software/be/geyser-proxy.sh
+++ api/v2/software/be/geyser-proxy.sh
@@ -2,9 +2,7 @@
 # Minecraft Server auto stop script - Do not configure this script!!
 # Version 3.0.0.0-#0 made by CrazyCloudCraft https://crazycloudcraft.de
 # shellcheck source=/dev/null
-cd ./../configs || exit 1
 source variables.sh
-cd $MTPATH || exit 1
 # Floodgate
 mkdir -p $MTPATH/mcsys/saves/floodgate
 cd $MTPATH/mcsys/saves/floodgate || exit 1
