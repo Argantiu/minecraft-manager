ea14b20131485cebc64929a76268a3b4a8a68753
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-12-05 09:29:25 +0100

    Update restart.sh

--- api/v2/main/restart.sh
+++ api/v2/main/restart.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Automatic minecraft server script - Edit at your own risks!!
 # Version 3.0.0.0-#0 created by CrazyCloudCraft https://crazycloudcraft.de
-# shellcheck source=/dev/null
-source variables.sh
+MTPATH=$(cat < mcsys.yml | grep "server-directory:" | cut -d ':' -f2 | tr -d " ")
+MPREFIX="\033[1;30m[\033[1;32mArgantiu\033[1;30m]\033[0;37m"
+MCNAME=$(cat < mcsys.yml | grep "systemname:" | cut -d ':' -f2)
+SHRESTART=$(cat < $MLANG | grep "restartsh.server.offline:" | cut -d ':' -d '"' -f2 | sed "s/%server_name%/$MCNAME/g" | sed "s/%prefix%/$MPREFIX/g")
 # Check, if server is offline. And then start the server.
-if ! screen -list | grep -q "$MCNAME"; then echo -e "$SHRESTART" && /bin/bash $LPATH/mcsys/start.sh && exit 0
+if ! screen -list | grep -q "$MCNAME"; then echo -e "$SHRESTART" && /bin/bash $MTPATH/mcsys/start.sh && exit 0
 else
 # Stop and then start the server.
 /bin/bash $MTPATH/mcsys/stop.sh
