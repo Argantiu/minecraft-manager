d001a053cf137c4f50687f0399756dfd35c8a7b1
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-12-09 09:29:56 +0100

    Update restart.sh

--- api/v2/main/restart.sh
+++ api/v2/main/restart.sh
@@ -7,9 +7,9 @@ MPREFIX="\033[1;30m[\033[1;32mArgantiu\033[1;30m]\033[0;37m"
 MCNAME=$(cat < mcsys.yml | grep "systemname:" | cut -d ':' -f2)
 SHRESTART=$(cat < $MLANG | grep "restartsh.server.offline:" | cut -d ':' -d '"' -f2 | sed "s/%server_name%/$MCNAME/g" | sed "s/%prefix%/$MPREFIX/g")
 # Check, if server is offline. And then start the server.
-if ! screen -list | grep -q "$MCNAME"; then echo -e "$SHRESTART" && /bin/bash $MTPATH/mcsys/start.sh && exit 0
+if ! screen -list | grep -q "$MCNAME"; then echo -e "$SHRESTART" && /bin/bash "$MTPATH"/mcsys/start.sh && exit 0
 else
 # Stop and then start the server.
-/bin/bash $MTPATH/mcsys/stop.sh
-/bin/bash $MTPATH/mcsys/start.sh
+/bin/bash "$MTPATH"/mcsys/stop.sh
+/bin/bash "$MTPATH"/mcsys/start.sh
 fi
