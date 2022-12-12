37027827480bf96178d474e3fa30dbd6101f4f64
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-12-12 08:43:23 +0100

    Update geyser.sh

--- api/v2/software/be/geyser.sh
+++ api/v2/software/be/geyser.sh
@@ -8,24 +8,24 @@ PROXYMO=$(cat < mcsys.yml | grep "proxy-mode:" | cut -d ':' -f2)
 if [[ $VARSOFT == "MOHIST" ]] || [[ $VARSOFT == "mohist" ]] || [[ $VARSOFT == "mohistmc" ]]; then ASOFTWARE=modded/mohist.sh
 fi
 # Floodgate
-mkdir -p $MTPATH/mcsys/floodgate
-if [ ! $ASOFTWARE = "modded/mohist.sh" ]; then
- if [ ! -f $MTPATH/plugins/floodgate-spigot.jar ]; then touch $MTPATH/plugins/floodgate-spigot.jar
+mkdir -p "$MTPATH"/mcsys/floodgate
+if [ ! "$ASOFTWARE" = "modded/mohist.sh" ]; then
+ if [ ! -f "$MTPATH"/plugins/floodgate-spigot.jar ]; then touch "$MTPATH"/plugins/floodgate-spigot.jar
  fi
- cd $MTPATH/mcsys/floodgate || exit 1
+ cd "$MTPATH"/mcsys/floodgate || exit 1
  wget -q https://ci.opencollab.dev/job/GeyserMC/job/Floodgate/job/master/lastSuccessfulBuild/artifact/spigot/target/floodgate-spigot.jar -O floodgate-spigot.jar
  unzip -qq -t floodgate-spigot.jar
  if [ "$?" -ne 0 ]; then
-  echo "Downloaded floodgate default is corrupt. No update." | /usr/bin/logger -t $MCNAME
+  echo "Downloaded floodgate default is corrupt. No update." | /usr/bin/logger -t "$MCNAME"
  else
-  diff -q floodgate-spigot.jar $MTPATH/plugins/floodgate-spigot.jar >/dev/null 2>&1
+  diff -q floodgate-spigot.jar "$MTPATH"/plugins/floodgate-spigot.jar >/dev/null 2>&1
   if [ "$?" -eq 1 ]; then
    cp floodgate-spigot.jar floodgate-spigot.jar."$(date +%Y.%m.%d.%H.%M.%S)"
-   mv floodgate-spigot.jar $MTPATH/plugins/floodgate-spigot.jar
-   /usr/bin/find $MTPATH/mcsys/saves/floodgate/* -type f -mtime +5 -delete 2>&1 | /usr/bin/logger -t $MCNAME
-   echo "floodgate default has been updated" | /usr/bin/logger -t $MCNAME
+   mv floodgate-spigot.jar "$MTPATH"/plugins/floodgate-spigot.jar
+   /usr/bin/find "$MTPATH"/mcsys/saves/floodgate/* -type f -mtime +5 -delete 2>&1 | /usr/bin/logger -t "$MCNAME"
+   echo "floodgate default has been updated" | /usr/bin/logger -t "$MCNAME"
   else
-   echo "No floodgate default update neccessary" | /usr/bin/logger -t $MCNAME
+   echo "No floodgate default update neccessary" | /usr/bin/logger -t "$MCNAME"
    rm floodgate-spigot.jar
   fi
  fi
@@ -33,24 +33,24 @@ else echo -e "Bedrock support doesn't work on this software! Please use an other
 fi
 
 # Geyser
-mkdir -p $MTPATH/mcsys/geyser
+mkdir -p "$MTPATH"/mcsys/geyser
 if [[ ! $ASOFTWARE == "modded/mohist.sh" ]] && [[ ! $PROXYMO == "true" ]] || [[ ! $PROXYMO == "TRUE" ]]; then
- if [ ! -f $MTPATH/plugins/Geyser-Spigot.jar ]; then touch $MTPATH/plugins/Geyser-Spigot.jar
+ if [ ! -f "$MTPATH"/plugins/Geyser-Spigot.jar ]; then touch "$MTPATH"/plugins/Geyser-Spigot.jar
  fi
- cd $MTPATH/mcsys/geyser || exit 1
+ cd "$MTPATH"/mcsys/geyser || exit 1
  wget -q https://ci.opencollab.dev/job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/target/Geyser-Spigot.jar -O Geyser-Spigot.jar
  unzip -qq -t Geyser-Spigot.jar
  if [ "$?" -ne 0 ]; then
-  echo "Downloaded Geyser Default is corrupt. No update." | /usr/bin/logger -t $MCNAME
+  echo "Downloaded Geyser Default is corrupt. No update." | /usr/bin/logger -t "$MCNAME"
  else
-  diff -q Geyser-Spigot.jar $LPATH/plugins/Geyser-Spigot.jar >/dev/null 2>&1
+  diff -q Geyser-Spigot.jar "$LPATH"/plugins/Geyser-Spigot.jar >/dev/null 2>&1
   if [ "$?" -eq 1 ]; then
    cp Geyser-Spigot.jar Geyser-Spigot.jar."$(date +%Y.%m.%d.%H.%M.%S)"
-   mv Geyser-Spigot.jar $MTPATH/plugins/Geyser-Spigot.jar
-   /usr/bin/find $MTPATH/mcsys/saves/geyser/* -type f -mtime +5 -delete 2>&1 | /usr/bin/logger -t $MCNAME
-   echo "Geyser Default has been updated" | /usr/bin/logger -t $MCNAME
+   mv Geyser-Spigot.jar "$MTPATH"/plugins/Geyser-Spigot.jar
+   /usr/bin/find "$MTPATH"/mcsys/saves/geyser/* -type f -mtime +5 -delete 2>&1 | /usr/bin/logger -t "$MCNAME"
+   echo "Geyser Default has been updated" | /usr/bin/logger -t "$MCNAME"
   else
-   echo "No Geyser default update neccessary" | /usr/bin/logger -t $MCNAME
+   echo "No Geyser default update neccessary" | /usr/bin/logger -t "$MCNAME"
    rm Geyser-Spigot.jar
   fi
  fi
