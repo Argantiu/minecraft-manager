ce9204893a583b3446ca62b3fe0b3709bb49acb6
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-12-12 08:58:06 +0100

    Update installer.sh

--- api/v2/installer.sh
+++ api/v2/installer.sh
@@ -82,9 +82,9 @@ if ! command -v diff &> /dev/null; then apt-get install diffutils -y &> /dev/nul
 fi
 if ! command -v rpl &> /dev/null; then apt-get install rpl -y &> /dev/null
 fi
-sed -i "s:opt/.*:$DICTY:g" $DICTY/mcsys/configs/mcsys.yml >/dev/null 2>&1
-sed -i "0,:source variables.sh:s:$DICTY/mcsys/configs/variables.sh" $DICTY/mcsys/start.sh $DICTY/mcsys/stop.sh $DICTY/mcsys/restart.sh >/dev/null 2>&1
-sed -i "0,:source variables.sh:s:$DICTY/mcsys/configs/variables.sh" $DICTY/manage.tool >/dev/null 2>&1
+sed -i "s:opt/.*:$DICTY:g" "$DICTY"/mcsys/configs/mcsys.yml >/dev/null 2>&1
+sed -i "0,:source variables.sh:s:$DICTY/mcsys/configs/variables.sh" "$DICTY"/mcsys/start.sh "$DICTY"/mcsys/stop.sh "$DICTY"/mcsys/restart.sh >/dev/null 2>&1
+sed -i "0,:source variables.sh:s:$DICTY/mcsys/configs/variables.sh" "$DICTY"/manage.tool >/dev/null 2>&1
 joe "$DICTY"/mcsys/configs/mcsys.yml
 cd "$DICTY" || exit 1
 # download counter
