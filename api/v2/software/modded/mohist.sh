d0846e27c89b908a951d9e17094c4e03da163264
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-12-12 08:54:32 +0100

    Update mohist.sh

--- api/v2/software/modded/mohist.sh
+++ api/v2/software/modded/mohist.sh
@@ -6,26 +6,26 @@ MAINVERSION=$(cat < mcsys.yml | grep "version:" | cut -d ':' -f2)
 RAM=$(cat < mcsys.yml | grep "ram:" | cut -d ':' -d 'B' -f2)
 JAVABIN=$(cat < mcsys.yml | grep "java:" | cut -d ':' -f2)
 MCNAME=$(cat < mcsys.yml | grep "systemname:" | cut -d ':' -f2)
-cd $MTPATH/mcsys/saves/jar || exit 1
+cd "$MTPATH"/mcsys/saves/jar || exit 1
 DATE=$(date +%Y.%m.%d.%H.%M.%S)
-wget -q https://mohistmc.com/api/$MAINVERSION/latest/download -O mohist-$MAINVERSION-$DATE.jar
-unzip -qq -t  mohist-$MAINVERSION-$DATE.jar
+wget -q https://mohistmc.com/api/"$MAINVERSION"/latest/download -O mohist-"$MAINVERSION"-"$DATE".jar
+unzip -qq -t  mohist-"$MAINVERSION"-"$DATE".jar
 if [ "$?" -ne 0 ]; then
- echo "Downloaded mohist-$MAINVERSION-$DATE.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
+ echo "Downloaded mohist-$MAINVERSION-$DATE.jar is corrupt. No update." | /usr/bin/logger -t "$MCNAME"
 else
- diff -q mohist-$MAINVERSION-$DATE.jar $MTPATH/$MCNAME.jar >/dev/null 2>&1
+ diff -q mohist-"$MAINVERSION"-"$DATE".jar "$MTPATH"/"$MCNAME".jar >/dev/null 2>&1
  if [ "$?" -eq 1 ]; then
-  cp mohist-$MAINVERSION-$DATE.jar  mohist-$MAINVERSION-$DATE.jar.backup
-  mv mohist-$MAINVERSION-$DATE.jar $MTPATH/$MCNAME.jar
-  /usr/bin/find $MTPATH/mcsys/saves/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
-  echo "mohist-$MAINVERSION-$DATE.jar has been updated" | /usr/bin/logger -t $MCNAME
+  cp mohist-"$MAINVERSION"-"$DATE".jar  mohist-"$MAINVERSION"-"$DATE".jar.backup
+  mv mohist-"$MAINVERSION"-"$DATE".jar "$MTPATH"/"$MCNAME".jar
+  /usr/bin/find "$MTPATH"/mcsys/saves/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t "$MCNAME"
+  echo "mohist-$MAINVERSION-$DATE.jar has been updated" | /usr/bin/logger -t "$MCNAME"
  else
-  echo "No mohist-$MAINVERSION-$DATE.jar update neccessary" | /usr/bin/logger -t $MCNAME
-  rm mohist-$MAINVERSION-$DATE.jar
+  echo "No mohist-$MAINVERSION-$DATE.jar update neccessary" | /usr/bin/logger -t "$MCNAME"
+  rm mohist-"$MAINVERSION"-"$DATE".jar
  fi
 fi
 # Starting server
-cd $MTPATH || exit 1
-echo "Starting $MTPATH/$MCNAME.jar" | /usr/bin/logger -t $MCNAME
-screen -d -m -L -S $MCNAME  /bin/bash -c "$JAVABIN -Xms$RAM -Xmx$RAM -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar $MCNAME.jar nogui"
+cd "$MTPATH" || exit 1
+echo "Starting $MTPATH/$MCNAME.jar" | /usr/bin/logger -t "$MCNAME"
+screen -d -m -L -S "$MCNAME"  /bin/bash -c "$JAVABIN -Xms$RAM -Xmx$RAM -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar $MCNAME.jar nogui"
 exit 1
