4054ff3904f9864e652be23ee7cf66770b296d56
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-12-12 08:45:17 +0100

    Update paper.sh

--- api/v2/software/paper.sh
+++ api/v2/software/paper.sh
@@ -6,29 +6,29 @@ MAINVERSION=$(cat < mcsys.yml | grep "version:" | cut -d ':' -f2)
 RAM=$(cat < mcsys.yml | grep "ram:" | cut -d ':' -d 'B' -f2)
 JAVABIN=$(cat < mcsys.yml | grep "java:" | cut -d ':' -f2)
 MCNAME=$(cat < mcsys.yml | grep "systemname:" | cut -d ':' -f2)
-cd $MTPATH/mcsys/saves/jar || exit 1
+cd "$MTPATH"/mcsys/saves/jar || exit 1
 rm -f version.json
 wget -q https://api.papermc.io/v2/projects/paper/versions/"$MAINVERSION"/ -O version.json
 LATEST=$(cat < version.json | jq -r ".builds" | grep -v "," | grep -e "[0-9]" | tr -d " ")
-wget -q https://api.papermc.io/v2/projects/paper/versions/"$MAINVERSION"/builds/"$LATEST"/downloads/paper-"$MAINVERSION"-"$LATEST".jar -O paper-$MAINVERSION-$LATEST.jar
-unzip -qq -t paper-$MAINVERSION-$LATEST.jar
+wget -q https://api.papermc.io/v2/projects/paper/versions/"$MAINVERSION"/builds/"$LATEST"/downloads/paper-"$MAINVERSION"-"$LATEST".jar -O paper-"$MAINVERSION"-"$LATEST".jar
+unzip -qq -t paper-"$MAINVERSION"-"$LATEST".jar
 if [ "$?" -ne 0 ]; then 
-echo "Downloaded paper-$MAINVERSION-$LATEST.jar is corrupt. No update." | /usr/bin/logger -t $MCNAME
+echo "Downloaded paper-$MAINVERSION-$LATEST.jar is corrupt. No update." | /usr/bin/logger -t "$MCNAME"
 else 
-diff -q paper-$MAINVERSION-$LATEST.jar $MTPATH/$MCNAME.jar >/dev/null 2>&1
+diff -q paper-"$MAINVERSION"-"$LATEST".jar "$MTPATH"/"$MCNAME".jar >/dev/null 2>&1
  if [ "$?" -eq 1 ]; then 
-  cp paper-$MAINVERSION-$LATEST.jar paper-$MAINVERSION-$LATEST.jar."$(date +%Y.%m.%d.%H.%M.%S)" && mv paper-$MAINVERSION-$LATEST.jar $MTPATH/$MCNAME.jar
-  /usr/bin/find $MTPATH/mcsys/saves/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t $MCNAME
-  echo "paper-$MAINVERSION-$LATEST has been updated" | /usr/bin/logger -t $MCNAME
+  cp paper-"$MAINVERSION"-"$LATEST".jar paper-"$MAINVERSION"-"$LATEST".jar."$(date +%Y.%m.%d.%H.%M.%S)" && mv paper-"$MAINVERSION"-"$LATEST".jar "$MTPATH"/"$MCNAME".jar
+  /usr/bin/find "$MTPATH"/mcsys/saves/jar/* -type f -mtime +10 -delete 2>&1 | /usr/bin/logger -t "$MCNAME"
+  echo "paper-$MAINVERSION-$LATEST has been updated" | /usr/bin/logger -t "$MCNAME"
   rm -f version.json
  else 
-  echo "No paper-$MAINVERSION-$LATEST update neccessary" | /usr/bin/logger -t $MCNAME
-  rm paper-$MAINVERSION-$LATEST.jar
+  echo "No paper-$MAINVERSION-$LATEST update neccessary" | /usr/bin/logger -t "$MCNAME"
+  rm paper-"$MAINVERSION"-"$LATEST".jar
   rm -f version.json
  fi
 fi
 # Starting server.
-cd $MTPATH || exit 1
-echo "Starting $MTPATH/$MCNAME.jar" | /usr/bin/logger -t $MCNAME
-screen -d -m -L -S $MCNAME  /bin/bash -c "$JAVABIN -Xms$RAM -Xmx$RAM -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar $MCNAME.jar nogui"
+cd "$MTPATH" || exit 1
+echo "Starting $MTPATH/$MCNAME.jar" | /usr/bin/logger -t "$MCNAME"
+screen -d -m -L -S "$MCNAME"  /bin/bash -c "$JAVABIN -Xms$RAM -Xmx$RAM -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar $MCNAME.jar nogui"
 exit 1
