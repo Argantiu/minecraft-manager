1e6235b48ba98ca2b65870529dc9faaf5a37e619
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2023-01-21 08:27:26 +0100

    Create updater.sh

---
+++ api/v3/updater.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Version...
+#View for language in config
+
+# Check if mycsys.yml version has changed
+# mv mcsys.yml to mcsys.yml.old
+# Overtake settings
+# Remove mcsys.yml.old
+
+# Update main.sh
+# Update Software.sh
+
+# Check for if not proxy
+# Update Bedrock.sh
+# else update Bedrock.pr.sh
