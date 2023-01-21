f3c73edfb77444e5f0ea025bbe0b03fc55516330
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2023-01-21 08:20:09 +0100

    Update installer.sh

--- api/v3/installer.sh
+++ api/v3/installer.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+# Ask for language
+
+# Ask for server location
+# Ask for server Software
+# Downloading language assets
+# Downloading assets
+# Update packetloader
+# Update packets
+# Download needed packets
