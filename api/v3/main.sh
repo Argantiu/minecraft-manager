f22202bcea53648eb509d13b1d316e5013fe27d3
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2023-01-21 11:25:53 +0100

    Update main.sh

--- api/v3/main.sh
+++ api/v3/main.sh
@@ -5,7 +5,6 @@ MCNAME=$(yq eval '.name' mcsys.yml)
 MCPATH=$(yq eval '.directory' mcsys.yml && sed 's/\/$//')
 #MCCOUNT=yq eval '.count' mcsys.yml
 #MCSOFTWARE=$(yq eval '.software' mcsys.yml && yq 'downcase')
-#MCBACKUP=$(yq eval '.backup' mcsys.yml
 #MCBEDROCK=$(yq eval '.bedrock' mcsys.yml
 #MCPROXY=$(yq eval '.proxy' mcsys.yml
 
@@ -19,7 +18,20 @@ if [ ! -f "$MCPATH"/libraries/mcsys/updater.sh ]; then touch "$MCPATH"/libraries
 fi
 sed -i 's/false/true/g' "$MCPATH"/eula.txt >/dev/null 2>&1
 sed -i 's;restart-script: ./start.sh;restart-script: ./main.sh 3;g' "$MCPATH"/spigot.yml >/dev/null 2>&1
-
+MCBACKUP=$(yq eval '.backup' mcsys.yml
+if [[ $MCBACKUP == "true" ]]; then
+ if [ -f "$MCNAME.jar" ]; then echo -e "3"
+    cd "$MCPATH"/libraries/mcsys/backups && usr/bin/find "$MTPATH"/unused/backups/* -type f -mtime +10 -delete 2>&1 #ls -1tr | head -n -10 | xargs -d '\n' rm -f --
+    cd "$MCPATH" || exit 1
+    if [[ $BETTERBACKUP == "TRUE" ]] || [[ $BETTERBACKUP == "true" ]]; then
+    tar -pzcf ./unused/backups/backup-"$(date +%Y.%m.%d.%H.%M.%S)".tar.gz --exclude="unused/*" --exclude="$MCNAME.jar" --exclude="mcsys/*" --exclude="cache/*" --exclude="logs/*" --exclude="libraries/*" --exclude="paper.yml-README.txt" --exclude="screenlog.*" --exclude="versions/*" ./ 
+    echo -e "$MSTART4"
+    else
+    tar -pzcf ./unused/backups/backup-"$(date +%Y.%m.%d.%H.%M.%S)".tar.gz --exclude="unused/*" --exclude="$MCNAME.jar" --exclude="mcsys/saves/*" ./
+    echo -e "$MSTART4"
+    fi
+ fi
+fi
 # Make Backup
 # push Software.sh
 # Berock Part
