5853d315df00ff2957fa25d629b543719ad4ec2e
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2023-01-21 08:13:40 +0100

    Update main.sh

--- api/v3/main.sh
+++ api/v3/main.sh
@@ -1,1 +1,38 @@
 #!/bin/bash
+
+
+
+
+mcstart (){
+# Fix System errors.
+
+# Update Java if needed.
+
+# Make Backup
+# push Software.sh
+# Berock Part
+# Chage to proxy if needed
+# Logrotate
+# wait until software finish
+# Start Server
+}
+
+mcstop (){
+# Check if Server already offline
+# Check if can make counter
+# Stopps server
+}
+
+mcrestart (){
+# Check if server already offline
+# restart server
+}
+
+##### Choosemodul
+#-h or anythig = help
+# 1 = Start
+# 2 = Stop
+# 3 = Restart
+# 4 = Config
+# 5 = Delete
+# * = Help
