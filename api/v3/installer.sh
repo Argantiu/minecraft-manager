b5cd8beb07e9249515e80982b36a1956cbfd8346
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2023-01-21 15:16:19 +0100

    Update installer.sh

--- api/v3/installer.sh
+++ api/v3/installer.sh
@@ -9,3 +9,7 @@
 # Update packetloader
 # Update packets
 # Download needed packets
+
+apt install gnupg ca-certificates curl
+curl -s https://repos.azul.com/azul-repo.key | gpg --dearmor -o /usr/share/keyrings/azul.gpg
+echo "deb [signed-by=/usr/share/keyrings/azul.gpg] https://repos.azul.com/zulu/deb stable main" | sudo tee /etc/apt/sources.list.d/zulu.list
