7690b269e194dec8a7bfd161bdf3b05c10700512
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2023-01-21 15:18:18 +0100

    Update installer.sh

--- api/v3/installer.sh
+++ api/v3/installer.sh
@@ -13,3 +13,4 @@
 apt install gnupg ca-certificates curl
 curl -s https://repos.azul.com/azul-repo.key | gpg --dearmor -o /usr/share/keyrings/azul.gpg
 echo "deb [signed-by=/usr/share/keyrings/azul.gpg] https://repos.azul.com/zulu/deb stable main" | sudo tee /etc/apt/sources.list.d/zulu.list
+#apt install zulu17-jdk
