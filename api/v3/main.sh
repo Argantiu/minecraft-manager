e5ae50d694b24c3eb12a635fb9380acf41f2636a
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2023-01-22 09:43:53 +0100

    Update main.sh

--- api/v3/main.sh
+++ api/v3/main.sh
@@ -3,8 +3,6 @@
 MCPREFIX="\033[1;30m[\033[1;32mArgantiu\033[1;30m]\033[0;37m"
 MCNAME=$(yq eval '.name' mcsys.yml)
 MCPATH=$(yq eval '.directory' mcsys.yml && sed 's/\/$//')
-#MCCOUNT=yq eval '.count' mcsys.yml
-#MCSOFTWARE=$(yq eval '.software' mcsys.yml && yq 'downcase')
 
 mcstart (){
 if screen -list | grep -q "$MCNAME"; then echo -e "1" && exit 1
@@ -54,15 +52,51 @@ exit 0
 }
 
 mcstop (){
-# Check if Server already offline
-# Check if can make counter
-# Stopps server
+if ! screen -list | grep -q "$MCNAME"; then echo -e "S1"
+  exit 1
+fi
+MCCOUNT=$(yq eval '.count' mcsys.yml)
+MCSOFTWARE=$(yq eval '.software' mcsys.yml && yq 'downcase')
+if ! [[ $MCSOFTWARE == "bungeecord" ]] || [[ $MCSOFTWARE == "velocity" ]] || [[ $MCSOFTWARE == "waterfall" ]] && [[ $MCCOUNT == "true" ]]; then
+mkdir -p "$MCPATH"/cache/mcsys && cd "$MCPATH"/cache/mcsys || exit 1
+hostname -I > ip-info.txt
+MCIPAD=$(cat < ip-info.txt | grep -o '^\S*')
+MCPORT=$(cat < "$MTPATH"/server.properties | grep server-port= | cut -b 13,14,1)
+wget -q https://api.minetools.eu/ping/"$MCIPAD"/"$MCPORT" -O on-i.txt
+ if grep -q error "on-i.txt"; then echo -e "S3" 
+ else MCOTYPE=$(cat < on-i.txt | grep online | tr -d " " | cut -b 10)
+ fi
+fi
+if ! [[ $MCOTYPE = "0" ]]; then screen -Rd "$MCNAME" -X stuff "say S4 10 S5 $(printf '\r')" && sleep 6s
+ screen -Rd "$MCNAME" -X stuff "say S4 4 S5 $(printf '\r')" && sleep 1s
+ screen -Rd "$MCNAME" -X stuff "say S4 3 S5 $(printf '\r')" && sleep 1s
+ screen -Rd "$MCNAME" -X stuff "say S4 2 S5 $(printf '\r')" && sleep 1s
+ screen -Rd "$MCNAME" -X stuff "say S4 1 S5 $(printf '\r')" && sleep 1s
+fi
+screen -Rd "$MCNAME" -X stuff "say S6 $(printf '\r')"
+StopChecks=0
+while [ $StopChecks -lt 30 ]; do
+  if ! screen -list | grep -q "$MCNAME"; then
+    break
+  fi
+  sleep 1;
+  StopChecks=$((StopChecks+1))
+done
+if screen -list | grep -q "$MCNAME"; then
+  echo -e "S7"
+  screen -S "$MCNAME" -X quit
+  pkill -15 -f "SCREEN -dmSL $MCNAME"
+fi
+echo -e "S8"
 exit 0
 }
 
 mcrestart (){
-# Check if server already offline
-# restart server
+if ! screen -list | grep -q "$MCNAME"; then echo -e "S1 & 2" && mcstart && exit 0
+else
+mcstop
+mcstart
+fi
 exit 0
 }
 
