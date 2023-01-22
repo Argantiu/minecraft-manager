7f47d6391811870ea2b7b670b1ec769d23944feb
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2023-01-22 20:18:38 +0100

    Update test.sh

--- api/v3/test.sh
+++ api/v3/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# https://stackoverflow.com/questions/73494478/ansi-color-codes-with-jq
 MCPREFIX=$'\033[1;30m[\033[1;32mArgantiu\033[1;30m]\033[0;37m'
 #echo -e " $(jq -r .mcstart.online messages.json)"
-jq -nc --arg MCPREFIX "$MCPREFIX" '{"a":"b","c":$MCPREFIX}' | jq -r .c
+jq -nc --arg MCPREFIX "$MCPREFIX" '{"c":$MCPREFIX}' | jq -r .c
