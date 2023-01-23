bef6a052b14b5a2e280151b3f1ea5b7ec766637f
CloudlyBot <114483978+CloudlyBot@users.noreply.github.com>
2023-01-23 09:13:11 +0100

    Update test.sh

--- api/v3/test.sh
+++ api/v3/test.sh
@@ -3,3 +3,4 @@
 MCPREFIX=$'\033[1;30m[\033[1;32mArgantiu\033[1;30m]\033[0;37m'
 #echo -e " $(jq -r .mcstart.online messages.json)"
 jq -nc --arg MCPREFIX "$MCPREFIX" '.file.object | messages.json' | jq -r .object
+jq -nc --arg MCPREFIX "$MCPREFIX" '.mcstart.online' messages.json | jq -r .online
