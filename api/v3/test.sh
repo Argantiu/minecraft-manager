ea0b860929950c136bf5049f3c63a78435f54f0b
CloudlyBot <114483978+CloudlyBot@users.noreply.github.com>
2023-01-23 08:49:13 +0100

    [Changes] ...
    Added template for cmd jq

--- api/v3/test.sh
+++ api/v3/test.sh
@@ -2,4 +2,4 @@
 # https://stackoverflow.com/questions/73494478/ansi-color-codes-with-jq
 MCPREFIX=$'\033[1;30m[\033[1;32mArgantiu\033[1;30m]\033[0;37m'
 #echo -e " $(jq -r .mcstart.online messages.json)"
-jq -nc --arg MCPREFIX "$MCPREFIX" '{"c":$MCPREFIX}' | jq -r .c
+jq -nc --arg MCPREFIX "$MCPREFIX" '.file.object | messages.json' | jq -r .object
