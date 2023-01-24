6dc08e760799bb30a2df3c0a08a526ddc8147607
CloudlyBot <114483978+CloudlyBot@users.noreply.github.com>
2023-01-24 12:30:03 +0100

    Update software.sh

--- api/v3/software.sh
+++ api/v3/software.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [[ $(yq eval '.version' mcsys.yml && cut -d "." -f2) == "19" ]] || [[ $(yq eval '.version' mcsys.yml && cut -d "." -f2) == "18" ]]; then
+MCVERS=$(yq eval '.version' ./../../mcsys.yml && cut -d "." -f2)
+if [[ $MCVERS == "19" ]] || [[ $MCVERS == "18" ]]; then
 apt install zulu17-jdk
 else apt install zulu8-jdk
 fi
