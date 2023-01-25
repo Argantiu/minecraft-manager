e48cc4e4ef19cd797683bdc2bf6a527453ad0b91
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2023-01-25 19:40:48 +0100

    Update installer.sh

--- api/v3/installer.sh
+++ api/v3/installer.sh
@@ -11,9 +11,7 @@ echo -e "$PREFIX -----------------------------------"
 echo -e "$PREFIX First, please select your language:"
 echo -e "$PREFIX 1 = English (English)"
 echo -e "$PREFIX 2 = Deutsch (German)"
-{ echo -n -e "$PREFIX Please type a number and hit enter:";
-echo -n -e " "
-read -r LANG; }
+{ echo -n -e "$PREFIX Please type a number and hit enter:"; echo -n -e " "; read -r LANG; }
 case $LANG in
 1) echo -e "$PREFIX Where is your server directory located?"
  echo -e "$PREFIX e.g. /opt/paper or /home/myserver/server"
@@ -23,8 +21,7 @@ case $LANG in
  echo -e "$PREFIX Und wo ist oder soll der Ordner sein:" ;;
 *) echo "Please select a language! " && exit 1 ;;
 esac
-{ echo -n -e " "
-read -r DCI; }
+{ echo -n -e " "; read -r DCI; }
 DICTY=$(echo "$DCI" | sed 's/\/$//')
 case $LANG in
 1) echo -e "$PREFIX Wich Software do you want to use?"
@@ -35,8 +32,7 @@ esac
 { echo -n -e " "
 read -r ASOFT; }
 MCWARE=$(echo $ASOFT | tr '[:upper:]' '[:lower:]' | sed 's/mc//')
-if ! command -v wget $AGDEBUG; then apt-get install wget -y $AGDEBUG
-fi
+if ! command -v wget $AGDEBUG; then apt-get install wget -y $AGDEBUG; fi
 cd "$DICTY" || exit 1
 wget $ARGANTIUAPI/main.sh $AGDEBUG
 chmod +x ./main.sh
@@ -57,21 +53,14 @@ apt-get upgrade -y $AGDEBUG
 apt install gnupg ca-certificates curl -y $AGDEBUG
 curl -s https://repos.azul.com/azul-repo.key | gpg --dearmor -o /usr/share/keyrings/azul.gpg $AGDEBUG
 echo "deb [signed-by=/usr/share/keyrings/azul.gpg] https://repos.azul.com/zulu/deb stable main" | tee /etc/apt/sources.list.d/zulu.list $AGDEBUG
-if ! command -v joe $AGDEBUG; then apt-get install joe -y $AGDEBUG
-fi
-if ! command -v screen $AGDEBUG; then apt-get install screen -y $AGDEBUG
-fi
-if ! command -v sudo $AGDEBUG; then apt-get install sudo -y $AGDEBUG
-fi
-if ! command -v zip $AGDEBUG; then apt-get install zip -y $AGDEBUG
-fi
-if ! command -v xargs $AGDEBUG; then apt-get install findutils -y $AGDEBUG
-fi
-if ! command -v diff $AGDEBUG; then apt-get install diffutils -y $AGDEBUG
-fi
-if ! command -v rpl $AGDEBUG; then apt-get install rpl -y $AGDEBUG
-fi
-if ! command -v yq $AGDEBUG; then wget -q https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq && chmod +x /usr/bin/yq $AGDEBUG
+if ! command -v joe $AGDEBUG; then apt-get install joe -y $AGDEBUG; fi
+if ! command -v screen $AGDEBUG; then apt-get install screen -y $AGDEBUG; fi
+if ! command -v sudo $AGDEBUG; then apt-get install sudo -y $AGDEBUG; fi
+if ! command -v zip $AGDEBUG; then apt-get install zip -y $AGDEBUG; fi
+if ! command -v xargs $AGDEBUG; then apt-get install findutils -y $AGDEBUG; fi
+if ! command -v diff $AGDEBUG; then apt-get install diffutils -y $AGDEBUG; fi
+if ! command -v rpl $AGDEBUG; then apt-get install rpl -y $AGDEBUG; fi
+if ! command -v yq $AGDEBUG; then wget -q https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq && chmod +x /usr/bin/yq $AGDEBUG; fi
 sed -i "s|directory:.*|directory: $DICTY|g" "$DICTY"/mcsys.yml $AGDEBUG
 sed -i "s|software:.*|software: $MCWARE|g" "$DICTY"/mcsys.yml $AGDEBUG
 case $LANG in
