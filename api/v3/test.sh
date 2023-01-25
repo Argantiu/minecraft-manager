24085345f99adb19dae647d2be3f999d7f3ad5d9
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2023-01-25 18:47:11 +0100

    Delete test.sh

--- api/v3/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# https://stackoverflow.com/questions/73494478/ansi-color-codes-with-jq
-MCPREFIX=$'\033[1;30m[\033[1;32mArgantiu\033[1;30m]\033[0;37m'
-#echo -e " $(jq -r .mcstart.online messages.json)"
-jq -nc --arg MCPREFIX "$MCPREFIX" '.file.object | messages.json' | jq -r .object
-jq -nc --arg MCPREFIX "$MCPREFIX" '.mcstart.online' messages.json | jq -r .online
-
-#https://apihandyman.io/api-toolbox-jq-and-openapi-part-4-bonus-coloring-jqs-raw-output/
-echo '{ "greeting": "Hello", "who":"World" }' | jq -r '"\u001b[31m" + .greeting + "\u001b[0m " + .who'
