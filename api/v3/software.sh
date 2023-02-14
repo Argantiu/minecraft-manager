18692b1c043fbf6d88c692e1831b5233ecbd575b
CloudlyBot <114483978+CloudlyBot@users.noreply.github.com>
2023-02-14 14:08:56 +0100

    Update software.sh

--- api/v3/software.sh
+++ api/v3/software.sh
@@ -1,39 +1,45 @@
 #!/bin/bash
-PAPERAPI=https://api.papermc.io/v2/projects/paper/versions/
-PURPURAPI=https://api.purpurmc.org/v2/purpur/
+PMC=https://api.papermc.io/v2
+PAPERAPI="$PMC"/projects/paper/versions/
+PURPURAPI="$PMC"/purpur/
 SPIBUKAPI=https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 MOHISTAPI=https://mohistmc.com/api/
 BUNGEEAPI=https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target/BungeeCord.jar
-WATERAPI=https://api.papermc.io/v2/projects/waterfall/versions/
-VELOAPI=https://api.papermc.io/v2/projects/velocity/versions/
+WATERAPI="$PMC"/projects/waterfall/versions/
+VELOAPI="$PMC"/projects/velocity/versions/
 MCAPI=https://piston-data.mojang.com/v1/
 if [[ $(yq eval .debug ../../mcsys.yml) == "true" ]]; then MCDEBUG=&> /dev/null 2>&1; fi
-
 
 MCVERS=$(yq eval '.version' ./../../mcsys.yml && cut -d "." -f2)
 if [[ $MCVERS == "19" ]] || [[ $MCVERS == "18" ]]; then apt install zulu17-jdk; else apt install zulu8-jdk; fi
 
+MCVERSION=$(yq eval '.version' ./../../mcsys.yml)
+
 mcbase() { cd "$MCPATH"/libraries/mcsys/saves || exit 1
 rm -f version.json }
 
-paper() { mcbase && wget -q "$PAPERAPI""$MCVERSION"/ -O version.json
+paper() { 
+mcbase && wget -q "$PAPERAPI""$MCVERSION"/ -O version.json
 LATEST=$(cat < version.json | jq -r ".builds" | grep -v "," | grep -e "[0-9]" | tr -d " ")
 wget -q "$PAPERAPI""$MCVERSION"/builds/"$LATEST"/downloads/paper-"$MCVERSION"-"$LATEST".jar -O paper-"$MCVERSION"-"$LATEST".jar
-if [ "$(unzip -qq -t paper-"$MAINVERSION"-"$LATEST".jar)" -ne 0 ]; then 
-echo "Downloaded paper-$MAINVERSION-$LATEST.jar is corrupt. No update." $MCDEBUG
+if [ "$(unzip -qq -t paper-"$MCVERSION"-"$LATEST".jar)" -ne 0 ]; then 
+echo "Downloaded paper-$MCVERSION-$LATEST.jar is corrupt. No update." $MCDEBUG
 else 
-diff -q paper-"$MAINVERSION"-"$LATEST".jar "$MTPATH"/"$MCNAME".jar >/dev/null 2>&1
- if [ "$?" -eq 1 ]; then cp paper-"$MAINVERSION"-"$LATEST".jar paper-"$MAINVERSION"-"$LATEST".jar."$(date +%Y.%m.%d.%H.%M.%S)" && mv paper-"$MAINVERSION"-"$LATEST".jar "$MTPATH"/"$MCNAME".jar
-  /usr/bin/find "$MTPATH"/mcsys/saves/jar/* -type f -mtime +10 -delete 2>&1
-  echo "paper-$MAINVERSION-$LATEST has been updated" $MCDEBUG
+diff -q paper-"$MCVERSION"-"$LATEST".jar "$MTPATH"/"$MCNAME".jar >/dev/null 2>&1
+ if [ "$?" -eq 1 ]; then cp paper-"$MCVERSION"-"$LATEST".jar paper-"$MCVERSION"-"$LATEST".jar."$(date +%Y.%m.%d.%H.%M.%S)" && mv paper-"$MAINVERSION"-"$LATEST".jar "$MTPATH"/"$MCNAME".jar
+  /usr/bin/find "$MCPATH"/mcsys/saves/jar/* -type f -mtime +10 -delete 2>&1
+  echo "paper-$MCVERSION-$LATEST has been updated" $MCDEBUG
   rm -f version.json; else 
-  echo "No paper-$MAINVERSION-$LATEST update neccessary" $MCDEBUG
-  rm paper-"$MAINVERSION"-"$LATEST".jar
+  echo "No paper-$MCVERSION-$LATEST update neccessary" $MCDEBUG
+  rm paper-"$MCVERSION"-"$LATEST".jar
   rm -f version.json
  fi
 fi
+exit 0
 }
 
+purpur() {
+}
 
 
 
@@ -52,10 +58,6 @@ minecraft() {
 bukkit() {
 }
 spigot() {
-}
-paper() {
-}
-purpur() {
 }
 # Modded Server
 mohist() {
