9e3878bf4ff7e69451db850679eda13cb7e6cb4f
CloudlyBot <114483978+CloudlyBot@users.noreply.github.com>
2023-02-14 12:47:29 +0100

    Update updater.sh

--- api/v3/updater.sh
+++ api/v3/updater.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # V3.5.0.0
+CVERSION=1
 
 find / -name "mcagon" -exec rm {} \;
 
@@ -10,14 +11,20 @@ DIREC=$(yq eval '.directory' ../../mcsys.yml)
 cd "$DIREC" || exit 1
 if [[ $SYSMANG == "de" ]]; then
 wget -q https://raw.githubusercontent.com/Argantiu/minecraft-manager/dev-v3/api/v3/resources/de/mcsys.yml -O mcsys.yml.new
- if ! [[ $CONFVER == "1" ]]; then
- cat < mcsys.yml | sed -i "s|directory:.*|directory: |g" mcsys.yml.new
- 
-
-# Check if mycsys.yml version has changed
-# mv mcsys.yml to mcsys.yml.old
-# Overtake settings
-# Remove mcsys.yml.old
+else
+wget -q https://raw.githubusercontent.com/Argantiu/minecraft-manager/dev-v3/api/v3/resources/en/mcsys.yml -O mcsys.yml.new; fi
+if ! [[ $CONFVER == $CVERSION ]]; then
+sed -i "s|name:.*|name: $(yq eval '.name' mcsys.yml)|g" mcsys.yml.new
+sed -i "s|directory:.*|directory: $(yq eval '.directory' mcsys.yml)|g" mcsys.yml.new
+sed -i "s|version:.*|version: $(yq eval '.version' mcsys.yml)|g" mcsys.yml.new
+sed -i "s|software:.*|software: $(yq eval '.software' mcsys.yml)|g" mcsys.yml.new
+sed -i "s|bedrock:.*|bedrock: $(yq eval '.bedrock' mcsys.yml)|g" mcsys.yml.new
+sed -i "s|ram:.*|ram: $(yq eval '.ram' mcsys.yml)|g" mcsys.yml.new
+sed -i "s|proxy:.*|proxy: $(yq eval '.proxy' mcsys.yml)|g" mcsys.yml.new
+sed -i "s|counter:.*|counter: $(yq eval '.counter' mcsys.yml)|g" mcsys.yml.new
+sed -i "s|backup:.*|backup: $(yq eval '.backup' mcsys.yml)|g" mcsys.yml.new; fi
+rm mcsys.yml
+mv mcsys.yml.new mcsys.yml
 
 # Update main.sh
 # Update Software.sh
