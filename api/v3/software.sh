35633e808e812a9e593fb4ae28f510aa5549345f
CloudlyBot <114483978+CloudlyBot@users.noreply.github.com>
2023-02-18 09:39:10 +0100

    Update software.sh

--- api/v3/software.sh
+++ api/v3/software.sh
@@ -9,7 +9,7 @@ PAPERAPI="$PMC"/projects/paper/versions/
 #VELOAPI="$PMC"/projects/velocity/versions/
 #MCAPI=https://piston-data.mojang.com/v1/
 MCVERSION=$(yq eval '.version' ./../../mcsys.yml)
-
+RAM=$(yq eval '.ram' ./../../mcsys.yml)
 #if [[ $(yq eval .debug ../../mcsys.yml) == "true" ]]; then MCDEBUG=&> /dev/null 2>&1; fi
 
 MCVERS=$($MCVERSION && cut -d "." -f2)
@@ -38,6 +38,10 @@ diff -q paper-"$MCVERSION"-"$LATEST".jar "$MTPATH"/"$MCNAME".jar >/dev/null 2>&1
  fi
 fi
 rm -f version.json
+cd "$MCPATH" || exit 1
+echo "Starting $MCPATH/$MCNAME.jar"
+screen -d -m -L -S "$MCNAME"  /bin/bash -c "/usr/bin/java -Xms$RAM -Xmx$RAM -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar $MCNAME.jar nogui"
+exit 1
 exit 0
 }
 #purpur() velocity() waterfall() bungeecord() minecraft() bukkit() spigot() mohist()
