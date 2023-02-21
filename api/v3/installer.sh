9650d05e792d4a1a133315d7eab8cbf03a68b4be
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2023-02-21 11:23:34 +0100

    Update installer.sh

--- api/v3/installer.sh
+++ api/v3/installer.sh
@@ -11,19 +11,26 @@ echo -e "$MCPREFIX -----------------------------------"
 echo -e "$MCPREFIX First, please select your language:"
 echo -e "$MCPREFIX 1 = English (English)"
 echo -e "$MCPREFIX 2 = Deutsch (German)"
-{ echo -n -e "$MCPREFIX Please type a number and hit enter:"; echo -n -e " "; read -r LANG; }
-case $LANG in
+{ echo -n -e "$MCPREFIX Please type a number and hit enter:"; echo -n -e " "; read -r MCLANG; }
+case $MCLANG in
+1) echo -e "$MCPREFIX Do you want to view the install process?\n $MCPREFIX Please type true or false."
+2) echo -e "$MCPREFIX Willst du dir den instalationsprozess ansehen?\n $MCPREFIX Bitte schreibe true oder false."
+*) echo "Please select a language! " && exit 1 ;;
+{ echo -n -e " "; read -r MCDEB; }
+if [[ $MCDEB == "true" ]]; then MCDEBUG=>/dev/null 2>&1
+else MCDEBUG=
+fi
+case $MCLANG in
 1) echo -e "$MCPREFIX Where is your server directory located?"
  echo -e "$MCPREFIX e.g. /opt/paper or /home/myserver/server"
  echo -e "$MCPREFIX Your server directory:" ;;
 2) echo -e "$MCPREFIX Wo ist oder soll dein Serverordner sich befinnden?"
  echo -e "$MCPREFIX z.b. /opt/paper oder /home/meinserver/server"
  echo -e "$MCPREFIX Und wo ist oder soll der Ordner sein:" ;;
-*) echo "Please select a language! " && exit 1 ;;
 esac
 { echo -n -e " "; read -r DCI; }
 DICTY=$(echo "$DCI" | sed 's/\/$//')
-case $LANG in
+case $MCLANG in
 1) echo -e "$MCPREFIX Wich Software do you want to use?"
  echo -e "$MCPREFIX You can write [ paper, purpur, spigot, bukkit, mohist, bungeecord, velocity, waterfall ] here." ;;
 2) echo -e "$MCPREFIX Welche Software willst du verwenden?"
@@ -33,11 +40,11 @@ esac
 read -r ASOFT; }
 MCWARE=$(echo "$ASOFT" | tr '[:upper:]' '[:lower:]' | sed 's/mc//')
 mkdir -p "$DICTY"/libraries/mcsys
-if ! command -v wget; then apt-get install wget -y; fi
+if ! command -v wget $MCDEBUG; then apt-get install wget -y $MCDEBUG; fi
 cd "$DICTY" || exit 1
 wget -q $ARGANTIUAPI/main.sh
 chmod +x ./main.sh
-case $LANG in
+case $MCLANG in
 1) echo -e "$MCPREFIX Great! System is generating the configuration now. Please wait..."
   wget -q $ARGANTIUAPI/resources/de/mcsys.yml
   cd "$DICTY"/libraries/mcsys || exit 1
@@ -49,19 +56,19 @@ case $LANG in
 esac
 wget -q $ARGANTIUAPI/software.sh
 chmod +x ./software.sh
-apt-get -q update -y 
-apt-get -q upgrade -y
-apt install gnupg ca-certificates curl -y 
-curl -s https://repos.azul.com/azul-repo.key | gpg --dearmor -o /usr/share/keyrings/azul.gpg 
-echo "deb [signed-by=/usr/share/keyrings/azul.gpg] https://repos.azul.com/zulu/deb stable main" | tee /etc/apt/sources.list.d/zulu.list
-if ! command -v joe ; then apt-get install joe -y ; fi
-if ! command -v screen ; then apt-get install screen -y ; fi
-if ! command -v sudo ; then apt-get install sudo -y ; fi
-if ! command -v zip ; then apt-get install zip -y ; fi
-if ! command -v xargs ; then apt-get install findutils -y ; fi
-if ! command -v diff ; then apt-get install diffutils -y ; fi
-if ! command -v rpl ; then apt-get install rpl -y ; fi
-if ! command -v yq ; then wget -q https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq && chmod +x /usr/bin/yq; fi
+apt-get -q update -y $MCDEBUG 
+apt-get -q upgrade -y $MCDEBUG
+apt install gnupg ca-certificates curl -y $MCDEBUG
+curl -s https://repos.azul.com/azul-repo.key $MCDEBUG | gpg --dearmor -o /usr/share/keyrings/azul.gpg $MCDEBUG
+echo "deb [signed-by=/usr/share/keyrings/azul.gpg] https://repos.azul.com/zulu/deb stable main" $MCDEBUG | tee /etc/apt/sources.list.d/zulu.list $MCDEBUG
+if ! command -v joe $MCDEBUG; then apt-get install joe -y $MCDEBUG; fi
+if ! command -v screen $MCDEBUG; then apt-get install screen -y $MCDEBUG; fi
+if ! command -v sudo $MCDEBUG; then apt-get install sudo -y ; fi
+if ! command -v zip $MCDEBUG; then apt-get install zip -y $MCDEBUG; fi
+if ! command -v xargs $MCDEBUG; then apt-get install findutils -y $MCDEBUG; fi
+if ! command -v diff $MCDEBUG; then apt-get install diffutils -y $MCDEBUG; fi
+if ! command -v rpl $MCDEBUG; then apt-get install rpl -y $MCDEBUG; fi
+if ! command -v yq $MCDEBUG; then wget -q https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq && chmod +x /usr/bin/yq; fi
 sed -i "s|directory:.*|directory: $DICTY|g" "$DICTY"/mcsys.yml 
 sed -i "s|software:.*|software: $MCWARE|g" "$DICTY"/mcsys.yml 
 case $LANG in
