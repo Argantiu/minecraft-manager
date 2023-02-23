f6388b2e4dba599642bc757d197bf3d746928552
CloudlyBot <114483978+CloudlyBot@users.noreply.github.com>
2023-02-23 13:58:10 +0100

    Update terminal.sh

--- api/v3/terminal.sh
+++ api/v3/terminal.sh
@@ -2,4 +2,10 @@
 # Terminal for custom Commands
 # Coded for v3.5.5.0
 
+# 1. Commands start, stop, restart
+# 2. Setup more servers
+# 3. Addon Management cmd! mcadd
 
+sys-start(){ }
+sys-stop(){ }
+sys-restart(){ }
