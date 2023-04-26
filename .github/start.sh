d962bc2f3630084808826175153c4ec21c6c4bcd
CloudlyBot | Argantiu <114483978+CloudlyBot@users.noreply.github.com>
2023-04-26 10:04:47 +0200

    Update and rename oneline.sh to start.sh

--- .github/start.sh
+++ .github/start.sh
@@ -2,5 +2,5 @@
 MCJARNAME=folia-server-1.19.4-R0.1-SNAPSHOT
 RAM=2G
 MCNAME=mcserv
-JAVABIN=/usr/bin/java
+JAVABIN=/usr/local/sdkman/candidates/java/current/bin
 screen -d -m -L -S "$MCNAME"  /bin/bash -c "$JAVABIN -Xms$RAM -Xmx$RAM -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true --add-modules=jdk.incubator.vector -jar $MCJARNAME.jar nogui"
