63b734e1ce857c707565bab9c064f178b3fb3df3
Niklas <114483978+CloudlyBot@users.noreply.github.com>
2024-12-06 13:33:14 +0000

    chages to v4

--- api/test/player_check.sh
+++ api/test/player_check.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 IP=$(hostname -I | grep -o '^\S*') # the server ip. This can be 127.0.0.1
-PORT=$(cat < "$MCPATH"/server.properties | grep server-port= | cut -b 13,14,1) # get's server port !NOT OPTIMIZED
+PORT=$(cat < "$MCPATH"/server.properties | grep server-port | grep -oE '[0-9]+') # get's server port
+
+
 
 # Get server information
 # ... echo -e "\xFE\x01" | nc ... to get version and type
--- api/v4/main.sh
+++ api/v4/main.sh
@@ -5,6 +5,19 @@ source cycodly/VariableReader
 function logger() {
     echo -e "$(jq -r .$1 ./cycodly/messages.json | sed "s:%servername%:$MCNAME:g")"
 }
+function configRead() {
+    yq eval ".$1" mcsys.yml
+}
+EXCLUDE=(
+    "--exclude=${MCNAME}.jar"
+    "--exclude=cache/*"
+    "--exclude=logs/*"
+    "--exclude=libraries/*"
+    "--exclude=paper.yml-README.txt"
+    "--exclude=screenlog.*"
+    "--exclude=versions/*"
+)
+)
 
 
 # System functions
@@ -13,9 +26,19 @@ function start() {
     if screen -list | grep -q "$MCNAME"; then
         logger mcstart.online;
         return;
+    else
+        logger mcstart.start
     fi
-    logger mcstart.start
-    
+
+    if [$(configRead backup) == "true"]; then
+        logger mcstart.backup.create
+        mkdir -p "$MCPATH"/cycodly/backups
+        find "$MCPATH"/cycodly/backups/* -type f -mtime +10 -delete 2>&1
+        tar -pzcf "$MCPATH"/cycodly/backups/backup-"$MCNAME"-"$(date +%Y.%m.%d.%H.%M.%S)".tar.gz ${EXCLUDE[@]} ./
+        logger mcstart.backup.finish
+    fi
+    if []
+
 }
 
 function stop() {
